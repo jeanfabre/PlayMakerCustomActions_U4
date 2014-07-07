6f67c4b641df9b83d62ee0e996a40db6e8d55fe8
jeanfabre <contact@fabrejean.net>
2014-07-07 18:35:59 +0400

    New SwipeSprite action

---
+++ Assets/PlayMaker Custom Actions/Sprite/SwapSprite.cs
@@ -0,0 +1,70 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2014. All rights reserved.
+//--- __ECO__ __ACTION__
+
+using UnityEngine;
+using System.Collections;
+
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Animation)]
+	[Tooltip("Replaces a single Sprite on any GameObject. Object must have a Sprite Renderer.")]
+	public class SwapSprite : FsmStateAction
+	{
+		
+		[RequiredField]
+		[CheckForComponent(typeof(SpriteRenderer))]
+		public FsmOwnerDefault gameObject;
+		
+		public Sprite spriteToSwap;
+
+		public FsmBool resetOnExit;
+
+		private SpriteRenderer spriteRenderer;
+
+		Sprite _orig;
+
+		public override void Reset()
+		{
+			gameObject = null;
+			resetOnExit = false;
+
+			spriteToSwap = new Sprite();
+		}
+		
+		public override void OnEnter()
+		{
+			GameObject go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go == null) return;
+			
+			spriteRenderer = go.GetComponent<SpriteRenderer>();
+			
+			if (spriteRenderer == null)
+			{
+				LogError("SwapSingleSprite: Missing SpriteRenderer!");
+				return;
+			}
+
+			_orig = spriteRenderer.sprite;
+
+			SwapSprites();
+			
+			Finish();
+		}
+
+		public override void OnExit()
+		{
+			if (resetOnExit.Value)
+			{
+				spriteRenderer.sprite = _orig;
+			}
+		}
+		void SwapSprites()
+		{
+			spriteRenderer.sprite = spriteToSwap;
+		}
+
+
+		
+	}
+}
