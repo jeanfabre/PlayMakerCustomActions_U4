ec11b42fbd8ef02bcf3520ba8cb8f0e88354ddc9
jeanfabre <contact@fabrejean.net>
2015-07-07 07:43:13 +0300

    New custom action to remove  potential Byte Order Mark from string.

---
+++ Assets/PlayMaker Custom Actions/String/GetStringWithoutBOM.cs
@@ -0,0 +1,68 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2015. All rights reserved.
+// __ECO__ __ACTION__
+// http://hutonggames.com/playmakerforum/index.php?action=post;topic=10645.0;last_msg=50293
+// http://answers.unity3d.com/questions/10904/xmlexception-text-node-canot-appear-in-this-state.html
+
+using UnityEngine;
+using System.IO;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.String)]
+	[Tooltip("Gets a string without the Byte Order Mark. This can cause error for parsers like xml for example.")]
+	public class GetStringWithoutBOM : FsmStateAction
+	{
+		
+		[Tooltip("The string.")]
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+		public FsmString source;
+		
+		[Tooltip("The string without the BOM")]
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+		public FsmString result;
+		
+		
+		public override void Reset()
+		{
+			source = null;
+			result = null;
+			
+		}
+		
+		public override void OnEnter()
+		{
+			DoGetStringWithoutBOM();
+			
+			Finish();
+		}
+		
+		
+		void DoGetStringWithoutBOM()
+		{
+			if (source.IsNone) 
+			{
+				Debug.LogWarning("Source Variable not defined");
+				return;
+			}
+
+			if (result.IsNone) 
+			{
+				Debug.LogWarning("Result Variable not defined");
+				return;
+			}
+
+			MemoryStream memoryStream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(source.Value));
+			StreamReader streamReader = new StreamReader(memoryStream, true);
+			
+			string _result = streamReader.ReadToEnd();
+			
+			streamReader.Close();
+			memoryStream.Close();
+			
+			result.Value = _result;
+		}
+		
+	}
+}
