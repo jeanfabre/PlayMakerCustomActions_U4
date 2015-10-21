89447ee073614335d125a07bd9a2765563ed21e9
djaydino <djay_dino@hotmail.com>
2015-10-21 19:36:38 +0200

    Create GetWebcamName.cs

---
+++ Assets/PlayMaker Custom Actions/WebCam/GetWebcamName.cs
@@ -0,0 +1,40 @@
+// (c) copyright HutongGames, LLC 2010-2015. All rights reserved.
+// by Brzozowsky Adam (brzozowsky@gmail.com)
+// Forum : http://hutonggames.com/playmakerforum/index.php?topic=3880.msg53819#msg53819
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+using System.Collections;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory("WebCam")]
+    [Tooltip("Find webcam and store name as string")]
+    public class GetWebcamName : FsmStateAction
+    {
+        public FsmString[] storeWebcamName;
+		public FsmInt storeWebcamCount;
+		private FsmString[] camName;
+		private WebCamTexture webCamTexture;
+
+        public override void Reset()
+        {
+			storeWebcamName = null;
+			storeWebcamCount = 0;
+        }
+
+        public override void OnEnter()
+		{
+			int numOfCams = WebCamTexture.devices.Length;
+			this.camName = new FsmString[numOfCams];
+			storeWebcamCount.Value = numOfCams;
+			
+			for(int i = 0; i < numOfCams; i++)  
+			{  
+				this.camName[i] = WebCamTexture.devices[i].name;    
+				storeWebcamName[i].Value = camName[i].Value;
+			} 
+		Finish();
+		}
+	}
+}
