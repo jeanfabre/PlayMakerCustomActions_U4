01e7b863c64080ce5f3fc4a6ec6e2bc6a65f166d
djaydino <djay_dino@hotmail.com>
2015-11-03 08:17:38 +0100

    Update SetTransformParent.cs

--- Assets/PlayMaker Custom Actions/Transform/SetTransformParent.cs
+++ Assets/PlayMaker Custom Actions/Transform/SetTransformParent.cs
@@ -1,5 +1,5 @@
-﻿// (c) Copyright HutongGames, LLC 2010-2015. All rights reserved.
-//--- __ECO__ __ACTION__ ---//
+// (c) Copyright HutongGames, LLC 2010-2015. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
 
 using UnityEngine;
 
