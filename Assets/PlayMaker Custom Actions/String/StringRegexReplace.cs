78c05b2d914bd15bfbd40001597d1329d5c526f9
djaydino <djay_dino@hotmail.com>
2015-11-03 08:12:49 +0100

    Update StringRegexReplace.cs

--- Assets/PlayMaker Custom Actions/String/StringRegexReplace.cs
+++ Assets/PlayMaker Custom Actions/String/StringRegexReplace.cs
@@ -1,6 +1,6 @@
-﻿// (c) Copyright HutongGames, LLC 2010-2015. All rights reserved.
+// (c) Copyright HutongGames, LLC 2010-2015. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
 // http://hutonggames.com/playmakerforum/index.php?topic=7004.0
-// __ECO__ __ACTION__
 
 using UnityEngine;
 using System.Text.RegularExpressions;
