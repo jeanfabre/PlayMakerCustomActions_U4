6711818a5e45874a960d5c6784b096737d9332d7
djaydino <djay_dino@hotmail.com>
2015-11-03 07:54:01 +0100

    Update GameObjectIsActiveInHierarchy.cs

--- Assets/PlayMaker Custom Actions/Logic/GameObjectIsActiveInHierarchy.cs
+++ Assets/PlayMaker Custom Actions/Logic/GameObjectIsActiveInHierarchy.cs
@@ -1,5 +1,5 @@
-// (c) Copyright HutongGames, LLC 2010-2013. All rights reserved.
-/*--- __ECO__ __ACTION__ ---*/
+// (c) Copyright HutongGames, LLC 2010-2015. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
 
 using UnityEngine;
 using System.Collections;
