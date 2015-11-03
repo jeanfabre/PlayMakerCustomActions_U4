b6e7400744585e7727481252349ee4718d6e024a
djaydino <djay_dino@hotmail.com>
2015-11-03 08:11:51 +0100

    Update GetStringWithoutBOM.cs

--- Assets/PlayMaker Custom Actions/String/GetStringWithoutBOM.cs
+++ Assets/PlayMaker Custom Actions/String/GetStringWithoutBOM.cs
@@ -1,5 +1,5 @@
-﻿// (c) Copyright HutongGames, LLC 2010-2015. All rights reserved.
-// __ECO__ __ACTION__
+// (c) Copyright HutongGames, LLC 2010-2015. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
 // http://hutonggames.com/playmakerforum/index.php?action=post;topic=10645.0;last_msg=50293
 // http://answers.unity3d.com/questions/10904/xmlexception-text-node-canot-appear-in-this-state.html
 
