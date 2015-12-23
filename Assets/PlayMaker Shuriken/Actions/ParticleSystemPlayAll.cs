624140b4edbc3da4729af744a35547d7c0161f69
jeanfabre <contact@fabrejean.net>
2015-12-23 11:14:22 +0300

    new action ( from 2d platformer sample) ParticleSystemPlayAll

---
+++ Assets/PlayMaker Shuriken/Actions/ParticleSystemPlayAll.cs
@@ -0,0 +1,50 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2015. All rights reserved.
+//--- __ECO__ __PLAYMAKER__ __ACTION__ ---//
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory("Shuriken")]
+	[Tooltip("Plays All particles from a GameObject. Can include All children as well")]
+	public class ParticleSystemPlayAll : FsmStateAction
+	{
+		[RequiredField]
+		[Tooltip("The GameObject with particles")]
+		public FsmOwnerDefault gameObject;
+		
+
+		[Tooltip("Play All particles even on children")]
+		public FsmBool includeChilden;
+
+		
+		public override void Reset()
+		{
+			gameObject = null;
+			includeChilden = true;
+		}
+		
+		public override void OnEnter()
+		{
+			GameObject go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go != null)
+			{
+				foreach(ParticleSystem p in go.GetComponents<ParticleSystem>())
+				{
+					p.Play();
+				}
+
+				if (includeChilden.Value)
+				{
+					foreach(ParticleSystem p in go.GetComponentsInChildren<ParticleSystem>())
+					{
+						p.Play();
+					}
+				}
+			}
+
+			Finish();
+		}
+
+	}
+}
