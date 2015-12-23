6306ce84367a4f701941ea412c1ff99dd37ed3bd
jeanfabre <contact@fabrejean.net>
2015-12-23 11:31:33 +0300

    new action ( from 2d platformer sample) DestroyRoot

---
+++ Assets/PlayMaker Custom Actions/GameObject/DestroyRoot.cs
@@ -0,0 +1,51 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2015. All rights reserved.
+//--- __ECO__ __PLAYMAKER__ __ACTION__ ---//
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.GameObject)]
+	[Tooltip("Destroys a Game Object's transfom root.")]
+	public class DestroyRoot : FsmStateAction
+	{
+		[RequiredField]
+		[Tooltip("The GameObject to destroy its root.")]
+		public FsmOwnerDefault gameObject;
+		
+		[HasFloatSlider(0, 5)]
+		[Tooltip("Optional delay before destroying the Game Object.")]
+		public FsmFloat delay;
+
+		
+		public override void Reset()
+		{
+			gameObject = null;
+			delay = 0;
+		}
+		
+		public override void OnEnter()
+		{
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+
+			if (go != null)
+			{
+				if (delay.Value <= 0)
+				{
+					GameObject.Destroy(go.transform.root.gameObject);
+				}
+				else
+				{
+					GameObject.Destroy(go.transform.root.gameObject, delay.Value);
+				}
+				
+			
+			}
+			
+			Finish();
+		
+		}
+
+		
+	}
+}
