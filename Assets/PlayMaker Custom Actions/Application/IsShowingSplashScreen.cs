c6aa787d82f8ff71ae9356adb129e244f8e0a7d5
djaydino <djay_dino@hotmail.com>
2016-03-14 19:35:26 +0100

    Create IsShowingSplashScreen.cs
    a older action that never was placed on the eco system :)

---
+++ Assets/PlayMaker Custom Actions/Application/IsShowingSplashScreen.cs
@@ -0,0 +1,51 @@
+// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Application)]
+	[Tooltip("Sends Events if Is Showing Splash Screen or not")]
+	public class IsShowingSplashScreen : FsmStateAction
+	{
+        [Tooltip("Event to send if is Showing Splash Screen is True.")]
+		public FsmEvent isTrue;
+
+        [Tooltip("Event to send if is Showing Splash Screen is false.")]
+		public FsmEvent isFalse;
+
+        [Tooltip("Repeat every frame while the state is active.")]
+		public bool everyFrame;
+
+		public override void Reset()
+		{
+			isTrue = null;
+			isFalse = null;
+			everyFrame = false;
+		}
+
+		public override void OnEnter()
+		{
+#if !UNITY_5_0 && !UNITY_5_1
+			Fsm.Event(Application.isShowingSplashScreen ? isTrue : isFalse);
+#else
+	 Debug.Log("This Action only works on Unity 5.2 and upwards");
+#endif
+			
+			if (!everyFrame)
+			{
+			    Finish();
+			}
+		}
+		
+		public override void OnUpdate()
+		{
+#if !UNITY_5_0 && !UNITY_5_1
+			Fsm.Event(Application.isShowingSplashScreen ? isTrue : isFalse);
+#else
+			Debug.Log("This Action only works on Unity 5.2 and upwards");
+			Finish();
+#endif
+		}
+	}
+}
