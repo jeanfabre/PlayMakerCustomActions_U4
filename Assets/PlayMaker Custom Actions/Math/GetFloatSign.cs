2a841e32fc56b107fee80c0408f0093f3c3d0d08
Jean Fabre <contact@fabrejean.net>
2016-04-04 12:26:20 +0300

    new action to get the sign of a float as an int or a float, use Advanced feature to work on any types of updates

---
+++ Assets/PlayMaker Custom Actions/Math/GetFloatSign.cs
@@ -0,0 +1,66 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ 
+EcoMetaStart
+{
+	"script dependancies":[
+	   "Assets/PlayMaker Custom Actions/__internal/FsmStateActionAdvanced.cs"
+	   ]
+}
+EcoMetaEnd
+---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Math)]
+	[Tooltip("Store sign of a float as -1 or 1.")]
+	public class GetFloatSign : FsmStateActionAdvanced
+	{
+		[RequiredField]
+		[Tooltip("The float variable.")]
+		public FsmFloat floatVariable;
+
+
+		[UIHint(UIHint.Variable)]
+		[Tooltip("The sign as float. -1f or 1f")]
+		public FsmFloat sign;
+
+
+		[UIHint(UIHint.Variable)]
+		[Tooltip("The sign as int. -1 or 1")]
+		public FsmInt signAsInt;
+
+
+		public override void Reset()
+		{
+			base.Reset ();
+
+			floatVariable = new FsmFloat() {UseVariable =true};
+			sign = null;
+			signAsInt = null;
+
+		}
+		
+		public override void OnEnter()
+		{
+			OnActionUpdate ();
+			
+			if (!everyFrame)
+			{
+				Finish();
+			}
+		}
+		
+		public override void OnActionUpdate()
+		{
+
+
+
+			if (!sign.IsNone) sign.Value = Mathf.Sign(floatVariable.Value);
+
+			if (!signAsInt.IsNone) signAsInt.Value = floatVariable.Value == 0f ?1:System.Math.Sign(floatVariable.Value);
+		}
+
+	}
+}
