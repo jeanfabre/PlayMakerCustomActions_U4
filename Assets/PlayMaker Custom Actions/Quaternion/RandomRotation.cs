3b30d4391422bd13b8c71f3de5c83f754a3bd57d
Jean Fabre <contact@fabrejean.net>
2016-04-07 11:43:04 +0300

    new action RandomRotation

---
+++ Assets/PlayMaker Custom Actions/Quaternion/RandomRotation.cs
@@ -0,0 +1,50 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Math)]
+	[Tooltip("Sets a Quaternion Variable or vector3 variable to a random rotation.")]
+	public class RandomRotation : FsmStateAction
+	{
+
+		[UIHint(UIHint.Variable)]
+		[Tooltip("Store the random rotation")]
+		public FsmQuaternion storeResult;
+
+		[UIHint(UIHint.Variable)]
+		[Tooltip("Store the random rotation as euler angles")]
+		public FsmVector3 storeEulerResult;
+
+		[Tooltip("Seems to be related to the randomization algorythm, could provide better results when checked to avoid similar rotations over time.")]
+		public bool uniform;
+
+		Quaternion _quat;
+
+		public override void Reset()
+		{
+
+			storeResult = null;
+			storeEulerResult = null;
+			uniform = false;
+		}
+		
+		public override void OnEnter()
+		{
+
+			if (uniform)
+			{
+				_quat = Random.rotation;
+			}else{
+				_quat = Random.rotationUniform;
+			}
+
+			if (!storeResult.IsNone) storeResult.Value = _quat;
+			if (!storeEulerResult.IsNone) storeEulerResult.Value = _quat.eulerAngles;
+
+			Finish();
+		}
+	}
+}
