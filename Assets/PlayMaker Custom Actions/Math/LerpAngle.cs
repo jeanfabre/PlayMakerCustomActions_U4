916ef89e8c9d95163a9522f812af9fbbaac5a910
Jean Fabre <contact@fabrejean.net>
2016-05-02 10:18:30 +0300

    - new action LerpAngle taken from the forum

---
+++ Assets/PlayMaker Custom Actions/Math/LerpAngle.cs
@@ -0,0 +1,66 @@
+// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
+// __ECO__ __PLAYMAKER__ __ACTION__ 
+// http://hutonggames.com/playmakerforum/index.php?topic=12557.msg58654#msg58654
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+
+[ActionCategory (ActionCategory.Math)]
+	[Tooltip ("Allows lerp of angle value without problems when exceeding 360 degrees")]
+	public class LerpAngle : FsmStateAction
+	{
+		[RequiredField]
+		[Tooltip ("From Angle")]
+		public FsmFloat fromAngle;
+
+		[RequiredField]
+		[Tooltip ("To Angle")]
+		public FsmFloat toAngle;
+
+		[RequiredField]
+		[Tooltip ("Interpolate between FromFloat and ToFloat by this amount. Value is clamped to 0-1 range. 0 = FromFloat; 1 = ToFloat; 0.5 = half way in between.")]
+		public FsmFloat amount;
+
+		[RequiredField]
+		[UIHint (UIHint.Variable)]
+		[Tooltip ("Store the result in this float variable.")]
+		public FsmFloat LerpedAngle;
+
+		[Tooltip ("Repeat every frame. Useful if any of the values are changing.")]
+		public bool everyFrame;
+
+
+		public override void Reset ()
+		{
+			fromAngle = new FsmFloat { UseVariable = true };
+			toAngle = new FsmFloat { UseVariable = true };
+			LerpedAngle = null;
+			everyFrame = true;
+		}
+
+			
+		public override void OnEnter ()
+		{
+			DoAngleLerp ();
+
+			if (!everyFrame) {
+				Finish ();
+			}
+		}
+			
+		public override void OnUpdate ()
+		{
+			DoAngleLerp ();
+		}
+
+
+		void DoAngleLerp ()
+		{
+			LerpedAngle.Value = Mathf.LerpAngle (fromAngle.Value, toAngle.Value, amount.Value);
+
+		}
+	}
+
+}
