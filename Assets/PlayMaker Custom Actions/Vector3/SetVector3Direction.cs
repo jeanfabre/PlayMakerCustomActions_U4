bc857752d7a413c39b0392e86d692d1085d20e51
Jean Fabre <contact@fabrejean.net>
2016-05-19 11:24:47 +0300

    new action SetVector3Direction

---
+++ Assets/PlayMaker Custom Actions/Vector3/SetVector3Direction.cs
@@ -0,0 +1,58 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
+// __ECO__ __PLAYMAKER__ __ACTION__
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Vector3)]
+	[Tooltip("Sets the value of a Vector3 Variable using direction presets.")]
+	public class SetVector3Direction: FsmStateAction
+	{
+		public enum Vector3Direction {Up,Down,Left,Right,Forward,Back};
+
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+		[Tooltip("The vector3 to store direction")]
+		public FsmVector3 vector3Variable;
+
+		[RequiredField]
+		[Tooltip("The direction preset")]
+		public Vector3Direction direction;
+		
+		public override void Reset()
+		{
+			vector3Variable = null;
+			direction = Vector3Direction.Up;
+		}
+		
+		public override void OnEnter()
+		{
+			switch(direction)
+			{
+			case Vector3Direction.Up:
+				vector3Variable.Value = Vector3.up;
+				break;
+			case Vector3Direction.Down:
+				vector3Variable.Value = Vector3.down;
+				break;
+			case Vector3Direction.Left:
+				vector3Variable.Value = Vector3.left;
+				break;
+			case Vector3Direction.Right:
+				vector3Variable.Value = Vector3.right;
+				break;
+			case Vector3Direction.Forward:
+				vector3Variable.Value = Vector3.forward;
+				break;
+			case Vector3Direction.Back:
+				vector3Variable.Value = Vector3.back;
+				break;
+			}
+
+			Finish();		
+		}
+
+	}
+}
+
