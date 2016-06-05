b15fe659a027b1b78b716e7b7adbf74dec709e85
djaydino <djay_dino@hotmail.com>
2016-06-06 01:40:48 +0200

    Create GetSpriteColorRGBA.cs

---
+++ Assets/PlayMaker Custom Actions/Color/GetSpriteColorRGBA.cs
@@ -0,0 +1,90 @@
+// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
+// made by DjayDino
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+using System.Collections;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Color)]
+	[Tooltip("Sets the color RGBA of sprite renderer")]
+	public class GetSpriteColorRGBA : FsmStateAction
+	{
+		
+
+		public FsmOwnerDefault gameObject;
+
+		[UIHint(UIHint.Variable)]
+		[Tooltip("The Color of the UI component")]
+		public FsmColor color;
+		
+		[UIHint(UIHint.Variable)]
+        [Tooltip("Store the red channel in a float variable.")]
+		public FsmFloat red;
+		
+		[UIHint(UIHint.Variable)]
+        [Tooltip("Store the green channel in a float variable.")]
+		public FsmFloat green;
+		
+		[UIHint(UIHint.Variable)]
+        [Tooltip("Store the blue channel in a float variable.")]
+		public FsmFloat blue;
+		
+		[UIHint(UIHint.Variable)]
+        [Tooltip("Store the alpha channel in a float variable.")]
+		public FsmFloat alpha;
+
+        [Tooltip("Repeat every frame. Useful if the color variable is changing.")]		
+		public FsmBool everyFrame;
+
+
+		private SpriteRenderer sRenderer;
+		private GameObject go;
+		
+		public override void Reset()
+		{
+			gameObject = null;
+			red = null;
+			green = null;
+			blue = null;
+			alpha = null;
+			everyFrame = false;
+		
+		}
+		
+		public override void OnEnter()
+		{
+			go = Fsm.GetOwnerDefaultTarget(gameObject);
+			sRenderer = go.GetComponent<SpriteRenderer>();
+
+			GetSpriteColo();
+			
+			if (!everyFrame.Value)
+				Finish();	
+
+		}
+		
+		public override void OnUpdate()
+		{
+			GetSpriteColo();
+		}
+		
+		void GetSpriteColo()
+		{
+			if (sRenderer == null) return;
+			
+			var spriteColor = sRenderer.color;
+			
+			color.Value = spriteColor;
+			red.Value = spriteColor.r;
+			green.Value = spriteColor.g;
+			blue.Value = spriteColor.b;
+			alpha.Value = spriteColor.a;
+
+		}
+		
+	
+		
+	}
+}
