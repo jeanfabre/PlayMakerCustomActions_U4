c9cb95552f80c7e04e996d46190585f16fd94d1a
Jean Fabre <contact@fabrejean.net>
2016-06-06 11:52:24 +0300

    new get set angular velocity actions

---
+++ Assets/PlayMaker Custom Actions/Physics/GetAngularVelocity.cs
@@ -0,0 +1,106 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+// based on http://hutonggames.com/playmakerforum/index.php?topic=2493.0
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Physics)]
+	[Tooltip("Gets the Angular Velocity of a Game Object and stores it in a Vector3 Variable or each Axis in a Float Variable. NOTE: The Game Object must have a Rigid Body.")]
+	public class GetAngularVelocity : ComponentAction<Rigidbody> {
+		
+		[RequiredField]
+		[Tooltip("The Rigidbody GameObject with to get Angular Velocity from")]
+		[CheckForComponent(typeof(Rigidbody))]
+		public FsmOwnerDefault gameObject;
+
+		[Tooltip("The Angular Velocity")]
+		[UIHint(UIHint.Variable)]
+		public FsmVector3 vector;
+
+		[Tooltip("The Angular Velocity x axis value")]
+		[UIHint(UIHint.Variable)]
+		public FsmFloat x;
+
+		[Tooltip("The Angular Velocity y axis value")]
+		[UIHint(UIHint.Variable)]
+		public FsmFloat y;
+
+		[Tooltip("The Angular Velocity z axis value")]
+		[UIHint(UIHint.Variable)]
+		public FsmFloat z;
+
+		[Tooltip("The Angular Velocity value referencing")]
+		public Space space;
+
+		[Tooltip("Repeat every frame. Useful to watch value evolve over time")]
+		public bool everyFrame;
+
+		Vector3 _angularVelocity;
+
+		public override void Reset()
+		{
+			gameObject = null;
+			vector = null;
+			x = null;
+			y = null;
+			z = null;
+			space = Space.World;
+			everyFrame = false;
+		}
+
+		public override void Awake()
+		{
+			Fsm.HandleFixedUpdate = true;
+		}
+
+		public override void OnEnter()
+		{
+			DoGetAngularVelocity();
+			
+			if (!everyFrame)
+				Finish();		
+		}
+		
+		public override void OnFixedUpdate()
+		{
+			DoGetAngularVelocity();
+		}
+		
+		void DoGetAngularVelocity()
+		{
+			if (!UpdateCache(Fsm.GetOwnerDefaultTarget(gameObject)))
+			{
+				return;
+			}
+
+			_angularVelocity = rigidbody.angularVelocity;
+			
+			if (space == Space.Self)
+			{
+				_angularVelocity = rigidbody.transform.InverseTransformDirection(_angularVelocity);
+			}
+
+			if (!vector.IsNone)
+			{
+				vector.Value = _angularVelocity;
+			}
+
+			if (!x.IsNone)
+			{
+				x.Value = _angularVelocity.x;
+			}
+
+			if (!y.IsNone)
+			{
+				y.Value = _angularVelocity.y;
+			}
+
+			if (!z.IsNone)
+			{
+				z.Value = _angularVelocity.z;
+			}
+		}
+	}
+}
---
+++ Assets/PlayMaker Custom Actions/Physics/SetAngularVelocity.cs
@@ -0,0 +1,107 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+// based on http://hutonggames.com/playmakerforum/index.php?topic=2493.0
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Physics)]
+	[Tooltip("Sets the Angular Velocity of a Game Object. To leave any axis unchanged, set variable to 'None'. NOTE: Game object must have a rigidbody.")]
+	public class SetAngularVelocity : ComponentAction<Rigidbody> 
+	{
+		[RequiredField]
+		[Tooltip("The Rigidbody GameObject with to set Angular Velocity")]
+		[CheckForComponent(typeof(Rigidbody))]
+		public FsmOwnerDefault gameObject;
+		
+		[UIHint(UIHint.Variable)]
+		[Tooltip("The Angular Velocity")]
+		public FsmVector3 vector;
+
+		[Tooltip("The x axis Angular Velocity")]
+		public FsmFloat x;
+
+		[Tooltip("The y axis Angular Velocity")]
+		public FsmFloat y;
+
+		[Tooltip("The z axis Angular Velocity")]
+		public FsmFloat z;
+		
+		public Space space;
+		
+		public bool everyFrame;
+		
+		public override void Reset()
+		{
+			gameObject = null;
+			vector = null;
+			// default axis to variable dropdown with None selected.
+			x = new FsmFloat { UseVariable = true };
+			y = new FsmFloat { UseVariable = true };
+			z = new FsmFloat { UseVariable = true };
+			space = Space.Self;
+			everyFrame = false;
+		}
+
+		public override void Awake()
+		{
+			Fsm.HandleFixedUpdate = true;
+		}
+
+		public override void OnEnter()
+		{
+			DoSetAngularVelocity();
+			
+			if (!everyFrame)
+			{
+				Finish();
+			}		
+		}
+		
+		public override void OnFixedUpdate()
+		{
+			DoSetAngularVelocity();
+			
+			if (!everyFrame)
+				Finish();
+		}
+		
+		void DoSetAngularVelocity()
+		{
+			if (!UpdateCache(Fsm.GetOwnerDefaultTarget(gameObject)))
+			{
+				return;
+			}
+			
+			// init position
+			
+			Vector3 angularVelocity;
+			
+			if (vector.IsNone)
+			{
+				angularVelocity = space == Space.World ?
+					rigidbody.angularVelocity : 
+					rigidbody.transform.InverseTransformDirection(rigidbody.angularVelocity);
+			}
+			else
+			{
+				angularVelocity = vector.Value;
+			}
+			
+			// override any axis
+			
+			if (!x.IsNone) angularVelocity.x = x.Value;
+			if (!y.IsNone) angularVelocity.y = y.Value;
+			if (!z.IsNone) angularVelocity.z = z.Value;
+			
+			// apply
+			
+			rigidbody.angularVelocity = 
+				space == Space.World ? 
+					angularVelocity : 
+					rigidbody.transform.TransformDirection(angularVelocity);
+		}
+		
+	}
+}
