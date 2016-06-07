ea8392383dab00ef832d5146d9bbd5d371cc11b7
Jean Fabre <contact@fabrejean.net>
2016-06-07 10:18:13 +0300

    new action to get newline and tab character conveniently

---
+++ Assets/PlayMaker Custom Actions/String/GetNewLineCharacter.cs
@@ -0,0 +1,36 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+using System;
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.String)]
+	[Tooltip("Gets the newline character, prevents having to copy paste from text editor. Use it in StringSplit action for example")]
+	public class GetNewLineCharacter : FsmStateAction
+	{
+		
+		[Tooltip("The newline string.")]
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+		public FsmString newLine;
+
+		
+		
+		public override void Reset()
+		{
+			newLine = null;
+			
+		}
+		
+		public override void OnEnter()
+		{
+			newLine.Value = Environment.NewLine;
+
+			Finish();
+		}
+		
+
+		
+	}
+}
---
+++ Assets/PlayMaker Custom Actions/String/GetTabCharacter.cs
@@ -0,0 +1,35 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+using System;
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.String)]
+	[Tooltip("Gets the tab character, prevents having to copy paste from text editor. Use it in StringSplit action for example")]
+	public class GetTabCharacter : FsmStateAction
+	{
+		
+		[Tooltip("The tab string.")]
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+		public FsmString tab;
+
+		
+		public override void Reset()
+		{
+			tab = null;
+			
+		}
+		
+		public override void OnEnter()
+		{
+			tab.Value = "\t";
+
+			Finish();
+		}
+		
+
+		
+	}
+}
