6e748c8625f3535897210d4cb704863e9b35100d
djaydino <djay_dino@hotmail.com>
2016-06-13 15:41:37 +0200

    Create GetKeyCodeToString.cs

---
+++ Assets/PlayMaker Custom Actions/Input/GetKeyCodeToString.cs
@@ -0,0 +1,51 @@
+// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
+// Made by Djaydino http://www.jinxtergames.com
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+ 
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Input)]
+	[Tooltip("Get KeyCode and set to string ")]
+	public class GetKeyCodeToString : FsmStateAction
+	{
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+		public FsmString storeResult;
+		
+		public FsmEvent sendEvent;
+		private KeyCode kcode;
+		private KeyCode newcode;
+
+		public override void Reset()
+		{
+			storeResult = null;
+		}
+
+		public override void OnUpdate()
+		{
+		detectpressedkey();
+		}
+		
+		public void detectpressedkey()
+		{
+			foreach(KeyCode kcode in System.Enum.GetValues(typeof(KeyCode)))
+			{
+			if (Input.GetKey(kcode))
+			{
+				newcode = kcode;
+			}
+				if (Input.anyKey)
+				{
+				storeResult.Value = newcode.ToString();
+				Fsm.Event(sendEvent);
+				}
+				
+			}
+			
+		}
+	}
+}
