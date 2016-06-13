382effd6c8706cdb72dab472b28723eb6c67c9f5
djaydino <djay_dino@hotmail.com>
2016-06-13 15:39:40 +0200

    Create GetKeyUpFromString.cs

---
+++ Assets/PlayMaker Custom Actions/Input/GetKeyUpFromString.cs
@@ -0,0 +1,42 @@
+// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
+// Made by Djaydino http://www.jinxtergames.com
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Input)]
+	[Tooltip("Sends an Event when the Keystring is released. Waring!! The String must be a valid KeyCode.")]
+	public class GetKeyUpFromString : FsmStateAction
+	{
+		[RequiredField]
+		[Tooltip("The keystring to test. Waring!! The String must be a valid KeyCode.")]
+		public FsmString keyCode;
+		public FsmEvent sendEvent;
+		[UIHint(UIHint.Variable)]
+		public FsmBool storeResult;
+		private KeyCode key;
+		
+		public override void Reset()
+		{
+			sendEvent = null;
+			keyCode = null;
+			storeResult = null;
+		}
+
+		public override void OnEnter()
+		{
+			key = (KeyCode)System.Enum.Parse(typeof(KeyCode), keyCode.Value) ;
+		}
+		
+		public override void OnUpdate()
+		{
+			bool keyUp = Input.GetKeyUp(key);
+			
+			if (keyUp)
+				Fsm.Event(sendEvent);
+			
+			storeResult.Value = keyUp;
+		}
+	}
+}
