618b2103279d9d58e0feb981619c2a543f1ecb3d
Jean Fabre <contact@fabrejean.net>
2016-06-22 10:58:41 +0300

    New Action TransformDirectionAdvanced

---
+++ Assets/PlayMaker Custom Actions/Transform/TransformDirectionAdvanced.cs
@@ -0,0 +1,68 @@
+// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__
+EcoMetaStart
+{
+"script dependancies":[
+						"Assets/PlayMaker Custom Actions/__internal/FsmStateActionAdvanced.cs"
+					]
+}
+EcoMetaEnd
+---*/
+
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Transform)]
+	[Tooltip("Transforms a Direction from a Game Object's local space to world space. This operation is not affected by scale or position of the transform. The returned vector has the same length as direction.")]
+	public class TransformDirectionAdvanced : FsmStateActionAdvanced
+	{
+		[RequiredField]
+		[Tooltip("The GameObject")]
+		public FsmOwnerDefault gameObject;
+
+		[RequiredField]
+		[Tooltip("The local direction")]
+		public FsmVector3 localDirection;
+
+
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+		[Tooltip("The local direction from gameObject expressed in world space")]
+		public FsmVector3 storeResult;
+
+		
+		public override void Reset()
+		{
+			base.Reset ();
+			gameObject = null;
+			localDirection = null;
+			storeResult = null;
+
+		}
+
+		public override void OnEnter()
+		{
+			OnActionUpdate ();
+			
+			if (!everyFrame)
+			{
+				Finish();
+			}
+		}
+
+	
+		public override void OnActionUpdate()
+		{
+			DoTransformDirection();
+		}
+		
+		void DoTransformDirection()
+		{
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if(go == null) return;
+			
+			storeResult.Value = go.transform.TransformDirection(localDirection.Value);
+		}
+	}
+}
+
