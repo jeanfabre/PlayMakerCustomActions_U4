d251b1083e633c656f610ca8beb062551ed8cfa3
Jean Fabre <contact@fabrejean.net>
2016-08-25 12:04:51 +0300

    pushed Gravity sample files

---
+++ Assets/PlayMaker Custom Samples/Quaternion/UnityLearnGravityScript/Scripts/GravityScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace HutongGames.PlayMaker.Ecosystem.Samples
+{
+	public class GravityScript : MonoBehaviour 
+	{
+		public Transform target;
+		
+		
+		void Update () 
+		{
+			Vector3 relativePos = (target.position + new Vector3(0, 1.5f, 0)) - transform.position;
+			Quaternion rotation = Quaternion.LookRotation(relativePos);
+			
+			Quaternion current = transform.localRotation;
+			
+			transform.localRotation = Quaternion.Slerp(current, rotation, Time.deltaTime);
+			transform.Translate(0, 0, 3 * Time.deltaTime);
+		}
+	}
+}
---
+++ Assets/PlayMaker Custom Samples/Quaternion/UnityLearnGravityScript/Scripts/GravityScriptAlaPlayMaker.cs
@@ -0,0 +1,62 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
+
+using UnityEngine;
+using System.Collections;
+
+namespace HutongGames.PlayMaker.Ecosystem.Samples
+{
+	/// <summary>
+	/// Gravity script ala PlayMaker. This script is the same as https://unity3d.com/learn/tutorials/topics/scripting/quaternions 
+	/// but reformated to be following how Actions where created to reproduce the exact same behaviour in PlayMaker.
+	/// So each action in the Gravity Fsm State are commented here for clarity.
+	/// </summary>
+	public class GravityScriptAlaPlayMaker : MonoBehaviour 
+	{
+
+		public GameObject Target;
+		public float Speed = 3f;
+		public Vector3 Target_Offset = new Vector3(0,1.5f,0);
+
+		Vector3 Current_Position;
+		Quaternion Current_Rotation;
+		float Delta_Time;
+		Quaternion Look_Rotation;
+		Quaternion New_Rotation;
+		Vector3 Relative_Position;
+		Vector3 Target_Position;
+		
+		void Update () 
+		{
+
+			// ACTION: Get Position
+			Target_Position = Target.transform.position;
+
+			// ACTION: Get Position
+			Current_Position = transform.position;
+
+			// ACTION: Vector3 Add
+			Target_Position += Target_Offset;
+
+			// ACTION: Vector3 Operator
+			Relative_Position = Target_Position - Current_Position;
+
+			// ACTION: Quaternion Look Rotation
+			Look_Rotation = Quaternion.LookRotation(Relative_Position);
+
+			// ACTION: Get Rotation
+			Current_Rotation = transform.localRotation;
+
+			// ACTION: GetTime
+			Delta_Time = Time.deltaTime;
+
+			// ACTION: Quaternion Slerp
+			New_Rotation = Quaternion.Slerp(Current_Rotation, Look_Rotation, Delta_Time );
+
+			// ACTION: Set Rotation
+			transform.localRotation = New_Rotation;
+
+			// ACTION: Translate
+			transform.Translate(0, 0, Speed * Delta_Time);
+		}
+	}
+}
