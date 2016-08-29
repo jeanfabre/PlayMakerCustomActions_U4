bac028c656d2bb1f22aafafbfc836c8ff3853937
Jean Fabre <contact@fabrejean.net>
2016-08-29 13:29:21 +0300

    moved actions from U3 to U4 rep

---
+++ Assets/PlayMaker Custom Actions/StateMachine/GetHost.cs
@@ -0,0 +1,28 @@
+// (c) Copyright HutongGames, LLC 2010-2014. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.GameObject)]
+	[Tooltip("Gets the fsm name of the host  when runnin as a sub Fsm")]
+	public class GetHost : FsmStateAction
+	{
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+		public FsmString hostName;
+
+		public override void Reset()
+		{
+			hostName = null;
+		}
+		
+		public override void OnEnter()
+		{
+			hostName.Value = Fsm.Host.Name;
+			
+			Finish();
+		}
+	}
+}
---
+++ Assets/PlayMaker Custom Actions/StateMachine/GetHostInfo.cs
@@ -0,0 +1,70 @@
+// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.StateMachine)]
+	[Tooltip("Gets information from the Host when a template is running as a Sub FSM.")]
+	public class GetHostInfo : FsmStateAction
+	{
+		[UIHint(UIHint.Variable)]
+		public FsmString hostFsmName;
+
+		[UIHint(UIHint.Variable)]
+		public FsmString gameObjectName;
+
+		[UIHint(UIHint.Variable)]
+		public FsmGameObject GameObject;
+
+		[UIHint(UIHint.Variable)]
+		public FsmString currentStateName;
+
+		public bool everyFrame;
+
+		public override void Reset()
+		{
+			hostFsmName = null;
+			gameObjectName = null;
+			GameObject = null;
+			currentStateName = null;
+			everyFrame = false;
+		}
+
+		public override void OnEnter()
+		{
+			GetInfo();
+			
+			if (!everyFrame)
+			{
+				Finish();
+			}
+		}
+
+		public override void OnUpdate()
+		{
+			GetInfo();
+		}
+
+		public void GetInfo()
+		{
+			if (! hostFsmName.IsNone){
+				hostFsmName.Value = Fsm.Host.Name;
+			}
+
+			if ( ! GameObject.IsNone)
+			{
+                GameObject.Value = Fsm.Host.GameObject;
+			}
+
+			if (! gameObjectName.IsNone){
+				gameObjectName.Value = Fsm.Host.GameObjectName;
+			}
+
+			if (! currentStateName.IsNone){
+				currentStateName.Value = Fsm.Host.ActiveStateName;
+			}
+		}
+	}
+}
