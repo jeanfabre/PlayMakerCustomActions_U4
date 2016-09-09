4b5833befe78f5493f516e49f3e617b00fc6eec3
Jean Fabre <contact@fabrejean.net>
2016-09-09 12:17:28 +0300

    - cleaned up obsolete custom actions - imported 1.8.3, compiles ok.

---
+++ Assets/StaticTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaticTest : MonoBehaviour {
+
+	public static float test;
+
+	public float value;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		value = StaticTest.test;
+	}
+}
