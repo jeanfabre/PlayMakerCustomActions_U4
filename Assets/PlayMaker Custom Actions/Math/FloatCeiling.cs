8eee30c59d69e068e57840d78197b4d679a01e93
Jean Fabre <contact@fabrejean.net>
2016-09-26 10:40:05 +0300

    added round up keywords for float ceiling for better search results

--- Assets/PlayMaker Custom Actions/Math/FloatCeiling.cs
+++ Assets/PlayMaker Custom Actions/Math/FloatCeiling.cs
@@ -1,5 +1,6 @@
-// (c) Copyright HutongGames, LLC 2010-2015. All rights reserved.
+// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
 /*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+/* Keywords : round up */
 
 using UnityEngine;
 
