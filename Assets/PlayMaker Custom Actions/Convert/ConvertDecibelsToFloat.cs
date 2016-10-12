383a9c3971b126ca028881054aa226d52e048fe4
Jean Fabre <contact@fabrejean.net>
2016-10-12 11:00:46 +0300

    new actions to convert decibels to volume and back

---
+++ Assets/PlayMaker Custom Actions/Convert/ConvertDecibelsToFloat.cs
@@ -0,0 +1,56 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+// https://twitter.com/Wahooney/status/785798323038937088
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Convert)]
+	[Tooltip("Converts Full Scale decibels (DBFS) to a volume value.")]
+	[HelpUrl("http://www.playdotsound.com/portfolio-item/decibel-db-to-float-value-calculator-making-sense-of-linear-values-in-audio-tools/")]
+	public class ConvertDecibelsToFloat : FsmStateAction
+	{
+
+		[RequiredField]
+
+		[HasFloatSlider(-80f,0f)]
+		[Tooltip("The Full Scale decibels (DBFS), Range from -80 to 0")]
+		public FsmFloat decibels;
+
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+		[Tooltip("The result Float volume from decibels.  range from 0 to 1")]
+		public FsmFloat volume;
+		
+		[Tooltip("Repeats every frame")]
+		public bool everyFrame;
+		
+		public override void Reset()
+		{
+			volume = null;
+			decibels = null;
+			everyFrame = false;
+		}
+		
+		public override void OnEnter()
+		{
+			DoConvert();
+			
+			if (!everyFrame)
+			{
+				Finish();
+			}
+		}
+		
+		public override void OnUpdate()
+		{
+			DoConvert();
+		}
+		
+		void DoConvert()
+		{
+			volume.Value = Mathf.Pow(10f,decibels.Value/20f);	
+		}
+	}
+}
---
+++ Assets/PlayMaker Custom Actions/Convert/ConvertFloatToDecibels.cs
@@ -0,0 +1,62 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+// https://twitter.com/Wahooney/status/785798323038937088
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Convert)]
+	[Tooltip("Converts a volume value to Full Scale decibels (DBFS).")]
+	[HelpUrl("http://www.playdotsound.com/portfolio-item/decibel-db-to-float-value-calculator-making-sense-of-linear-values-in-audio-tools/")]
+	public class ConvertFloatToDecibels : FsmStateAction
+	{
+		
+		[RequiredField]
+		[HasFloatSlider(0,1)]
+		[Tooltip("The Float volume to convert to full scale decibels (DBFS). range from 0 to 1")]
+		public FsmFloat volume;
+		
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+		[Tooltip("The Full Scalce decibels (DBFS) result. Range from -80 to 0")]
+		public FsmFloat decibels;
+		
+		[Tooltip("Repeats every frame")]
+		public bool everyFrame;
+		
+		public override void Reset()
+		{
+			volume = null;
+			decibels = null;
+			everyFrame = false;
+		}
+		
+		public override void OnEnter()
+		{
+			DoConvert();
+			
+			if (!everyFrame)
+			{
+				Finish();
+			}
+		}
+		
+		public override void OnUpdate()
+		{
+			DoConvert();
+		}
+		
+		void DoConvert()
+		{
+			
+			if (volume.Value>0)
+			{
+				decibels.Value = 20f*Mathf.Log10(volume.Value);
+			}else{
+				decibels.Value  = -80f;
+			}
+			
+		}
+	}
+}
