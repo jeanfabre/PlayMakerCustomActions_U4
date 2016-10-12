dd684f8c0c49bcbc01416e95cb4938950416d0c4
Jean Fabre <contact@fabrejean.net>
2016-10-12 12:23:46 +0300

    enabled SetRigidBodyConstraint for ecosystem

--- Assets/PlayMaker Custom Actions/Physics/SetRigidBodyConstraints.cs
+++ Assets/PlayMaker Custom Actions/Physics/SetRigidBodyConstraints.cs
@@ -1,4 +1,6 @@
-// (c) Copyright HutongGames, LLC 2010-2012. All rights reserved.
+// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+// http://hutonggames.com/playmakerforum/index.php?topic=1888.0
 
 using UnityEngine;
 
