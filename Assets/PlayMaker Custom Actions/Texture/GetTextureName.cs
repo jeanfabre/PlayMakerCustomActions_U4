477c88f86067aac0af714ee7098fe3d9b1d0cc5e
Jean Fabre <contact@fabrejean.net>
2016-11-25 10:38:29 +0300

    new action GetTextureName

---
+++ Assets/PlayMaker Custom Actions/Texture/GetTextureName.cs
@@ -0,0 +1,44 @@
+// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory("Texture")]
+	[Tooltip("Get a texture's name ")]
+	public class GetTextureName : FsmStateAction
+	{
+
+		[Tooltip("The texture")]
+		public FsmTexture texture;
+
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+		[Tooltip("The texture name.")]
+		public FsmString textureName;
+
+		public override void Reset ()
+		{
+			texture = null;
+			textureName = null;
+
+		}
+
+		public override void OnEnter ()
+		{
+			DoGetMaterialTextureName ();
+
+			Finish ();
+		}
+
+		void DoGetMaterialTextureName ()
+		{
+			if (texture.Value != null) {			
+				textureName.Value = texture.Value.name;
+			}else{
+				textureName.Value = string.Empty;
+			}
+		}
+	}
+}
