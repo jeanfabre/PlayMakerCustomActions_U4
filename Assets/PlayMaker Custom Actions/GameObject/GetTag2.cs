189d9310488d118ac6b41c109a693624ab352861
Jean Fabre <contact@fabrejean.net>
2016-11-28 10:22:33 +0300

    new action getTag2 to get owner easily

---
+++ Assets/PlayMaker Custom Actions/GameObject/GetTag2.cs
@@ -0,0 +1,57 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.GameObject)]
+	[Tooltip("Gets a Game Object's Tag and stores it in a String Variable.")]
+	public class GetTag2 : FsmStateAction
+	{
+		[Tooltip("The GameObject to get the tag from.")]
+		[RequiredField]
+		public FsmOwnerDefault gameObject;
+
+		[Tooltip("The tag of the GameObject")]
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+		public FsmString storeResult;
+
+		[Tooltip("runs everyframe, useful if value is expected to change often")]
+		public bool everyFrame;
+		
+		private GameObject _go;
+		
+		public override void Reset()
+		{
+			gameObject = null;
+			storeResult = null;
+			everyFrame = false;
+		}
+		
+		public override void OnEnter()
+		{
+			DoGetTag();
+			
+			if (!everyFrame)
+				Finish();
+		}
+		
+		public override void OnUpdate()
+		{
+			DoGetTag();
+		}
+		
+		void DoGetTag()
+		{
+			_go = gameObject.OwnerOption == OwnerDefaultOption.UseOwner ? Owner : gameObject.GameObject.Value;
+
+			if (_go == null) 
+				return;
+			
+			storeResult.Value = _go.tag;
+		}
+		
+	}
+}
