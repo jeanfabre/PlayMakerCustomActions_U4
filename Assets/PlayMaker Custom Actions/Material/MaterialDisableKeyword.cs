4910f2e96b5146fa1ca26ad1814a0dd3ff938946
Jean Fabre <contact@fabrejean.net>
2016-12-01 09:30:16 +0300

    new actions to work with material keywords

---
+++ Assets/PlayMaker Custom Actions/Material/MaterialDisableKeyword.cs
@@ -0,0 +1,87 @@
+// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Material)]
+	[Tooltip("Sets a disableKeyword property of a material.")]
+	public class MaterialDisableKeyword : FsmStateAction
+	{
+		[Tooltip("The GameObject that the material is applied to.")]
+		[CheckForComponent(typeof(Renderer))]
+		public FsmOwnerDefault gameObject;
+
+		[Tooltip("GameObjects can have multiple materials. Specify an index to target a specific material.")]
+		public FsmInt materialIndex;
+
+		[Tooltip("Alternatively specify a Material instead of a GameObject and Index.")]
+		public FsmMaterial material;
+
+		[RequiredField]
+		[Tooltip("keyword to disable.")]
+		public FsmString keyword;
+
+		[Tooltip("Event sent if material was not found")]
+		public FsmEvent fail;
+
+		public override void Reset()
+		{
+			gameObject = null;
+			materialIndex = 0;
+			material = null;
+			keyword = null;
+			fail = null;
+		}
+
+		public override void OnEnter()
+		{
+			bool ok = DoSetMaterialProperty();
+
+			if (!ok) Fsm.Event(fail);
+
+			Finish();
+		}
+
+		bool DoSetMaterialProperty()
+		{
+
+			if (material.Value != null)
+			{
+				material.Value.DisableKeyword(keyword.Value);
+				return true;
+			}
+			
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go == null) return false;
+
+			if (go.renderer == null)
+			{
+				LogError("Missing Renderer!");
+				return false;
+			}
+			
+			if (go.renderer.material == null)
+			{
+				LogError("Missing Material!");
+				return false;
+			}
+			
+			if (materialIndex.Value == 0)
+			{
+				go.renderer.material.DisableKeyword(keyword.Value);
+				return true;
+			}
+			else if (go.renderer.materials.Length > materialIndex.Value)
+			{
+				var materials = go.renderer.materials;
+				materials[materialIndex.Value].DisableKeyword(keyword.Value);
+				go.renderer.materials = materials;	
+				return true;
+			}	
+
+			return false;
+		}
+	}
+}
---
+++ Assets/PlayMaker Custom Actions/Material/MaterialGetShaderKeywords.cs
@@ -0,0 +1,92 @@
+// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Material)]
+	[Tooltip("Gets the shaderKeywords property of a material.")]
+	public class MaterialGetShaderKeywords : FsmStateAction
+	{
+		[Tooltip("The GameObject that the material is applied to.")]
+		[CheckForComponent(typeof(Renderer))]
+		public FsmOwnerDefault gameObject;
+
+		[Tooltip("GameObjects can have multiple materials. Specify an index to target a specific material.")]
+		public FsmInt materialIndex;
+
+		[Tooltip("Alternatively specify a Material instead of a GameObject and Index.")]
+		public FsmMaterial material;
+
+		[RequiredField]
+		[Tooltip("shader keywords.")]
+		[UIHint(UIHint.Variable)]
+		[ArrayEditor(VariableType.String)]
+		public FsmArray shaderKeywords;
+
+		[Tooltip("Event sent if material was not found")]
+		public FsmEvent fail;
+
+		public override void Reset()
+		{
+			gameObject = null;
+			materialIndex = 0;
+			material = null;
+			shaderKeywords = null;
+
+			fail = null;
+		}
+
+		public override void OnEnter()
+		{
+			bool ok = DoSetMaterialProperty();
+
+			if (!ok) Fsm.Event(fail);
+
+			Finish();
+		}
+
+		bool DoSetMaterialProperty()
+		{
+			
+		
+			//Debug.Log(_vector4);
+			if (material.Value != null)
+			{
+				shaderKeywords.stringValues = material.Value.shaderKeywords;
+				return true;
+			}
+			
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go == null) return false;
+
+			if (go.renderer == null)
+			{
+				LogError("Missing Renderer!");
+				return false;
+			}
+			
+			if (go.renderer.material == null)
+			{
+				LogError("Missing Material!");
+				return false;
+			}
+			
+			if (materialIndex.Value == 0)
+			{
+				shaderKeywords.stringValues = go.renderer.material.shaderKeywords;
+				return true;
+			}
+			else if (go.renderer.materials.Length > materialIndex.Value)
+			{
+				var materials = go.renderer.materials;
+				shaderKeywords.stringValues =  materials[materialIndex.Value].shaderKeywords;
+				go.renderer.materials = materials;	
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
