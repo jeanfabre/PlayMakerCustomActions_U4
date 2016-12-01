88a96a6c4d32c9fa503577fc7061c7ebad2c4338
Jean Fabre <contact@fabrejean.net>
2016-12-01 11:30:01 +0300

    new action to move rotation and position of 2d rb

---
+++ Assets/PlayMaker Custom Actions/Physics2D/Rigidbody2dMovePosition.cs
@@ -0,0 +1,75 @@
+// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Physics2D)]
+	[Tooltip("Moves a RigidBody2d to a particular position. It's more efficient then accessing the transform of the gameObject and interacts properly with colliders and physics materials")]
+	public class RigidBody2dMovePosition : ComponentAction<Rigidbody2D>
+	{
+		[RequiredField]
+		[CheckForComponent(typeof(Rigidbody2D))]
+		[Tooltip("The rigid body to move.")]
+		public FsmOwnerDefault gameObject;
+
+		[Tooltip("The position to move to.")]
+		public FsmVector2 position;
+
+		[Tooltip("The position to move to as vector3.")]
+		public FsmVector3 orVector3position;
+
+		[Tooltip("Repeat every frame.")]
+		public bool everyFrame;
+		
+		public override void Reset ()
+		{
+			gameObject = null;
+			position = null;
+			orVector3position = new FsmVector3(){UseVariable=true};
+			everyFrame = true;
+		}
+
+		public override void Awake ()
+		{
+			Fsm.HandleFixedUpdate = true;
+		}
+
+		public override void OnEnter()
+		{
+			DoPosition();
+			
+			if (!everyFrame)
+			{
+				Finish();
+			}		
+		}
+
+		public override void OnFixedUpdate ()
+		{
+			DoPosition ();
+            
+			if (!everyFrame) {
+				Finish ();
+			}
+		}
+
+		void DoPosition ()
+		{
+
+			if (!UpdateCache (Fsm.GetOwnerDefaultTarget (gameObject))) {
+				return;
+			}
+
+			Vector2 _pos = position.Value;
+			if (!orVector3position.IsNone)
+			{
+				_pos.x = orVector3position.Value.x;
+				_pos.y = orVector3position.Value.y;
+			}
+
+			rigidbody2d.MovePosition (position.Value);
+		}
+
+	}
+}
---
+++ Assets/PlayMaker Custom Actions/Physics2D/Rigidbody2dMoveRotation.cs
@@ -0,0 +1,63 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2016. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Physics)]
+	[Tooltip("Rotates a RigidBody2d to a particular rotation. It's more efficient then accessing the transform of the gameObject and interacts properly with colliders and physics materials")]
+	public class RigidBody2dMoveRotation : ComponentAction<Rigidbody2D>
+	{
+		[RequiredField]
+		[CheckForComponent(typeof(Rigidbody2D))]
+		[Tooltip("The rigid body to rotate.")]
+		public FsmOwnerDefault gameObject;
+
+		[Tooltip("A angle rotation.")]
+		public FsmFloat rotation;
+
+		[Tooltip("Repeat every frame.")]
+		public bool everyFrame;
+
+		private Rigidbody rigidBody;
+		
+		public override void Reset ()
+		{
+			gameObject = null;
+			rotation = null;
+			everyFrame = true;
+		}
+
+		public override void Awake ()
+		{
+			Fsm.HandleFixedUpdate = true;
+		}
+		
+		public override void OnEnter ()
+		{
+			DoRotate ();
+			
+			if (!everyFrame) {
+				Finish ();
+			}
+				
+		}
+
+		public override void OnFixedUpdate ()
+		{
+			DoRotate ();
+           
+		}
+
+		void DoRotate ()
+		{
+			if (!UpdateCache (Fsm.GetOwnerDefaultTarget (gameObject))) {
+				return;
+			}
+
+			rigidbody2d.MoveRotation(rotation.Value);
+		}
+
+	}
+}
