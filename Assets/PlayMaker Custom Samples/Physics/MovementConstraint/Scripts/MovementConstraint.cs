43ab63435abc205d174f1b3327cde4f25f0af0c1
Jean Fabre <contact@fabrejean.net>
2016-12-06 11:52:16 +0300

    - moved Movement constraint sample from 3 to 4 - added c# script comparison

---
+++ Assets/PlayMaker Custom Samples/Physics/MovementConstraint/Scripts/MovementConstraint.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Com.HutongGames.Ecosystem
+{
+	[System.Serializable]
+	public class Done_Boundary 
+	{
+		public float xMin, xMax, zMin, zMax;
+	}
+
+	public class MovementConstraint : MonoBehaviour
+	{
+		public float speed;
+		public float tilt;
+		public Done_Boundary boundary;
+
+		void FixedUpdate ()
+		{
+			float moveHorizontal = Input.GetAxis ("Horizontal");
+			float moveVertical = Input.GetAxis ("Vertical");
+			
+			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+			GetComponent<Rigidbody>().velocity = movement * speed;
+			
+			GetComponent<Rigidbody>().position = new Vector3
+				(
+					Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
+					0.0f, 
+					Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+					);
+			
+			GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+		}
+	}
+}
