0cd33e2604a22dc6b765a95f01fe1971f12a06fc
djaydino <djaydino@gmail.com>
2017-01-31 18:28:45 +0100

    new actions for audio

---
+++ Assets/PlayMaker Custom Actions/Audio/AudioPlayAdvanced.cs
@@ -0,0 +1,169 @@
+// (c) Copyright HutongGames, LLC 2010-2017. All rights reserved.
+/*--- __ECO__ __ACTION__ ---*/
+// Made By : DjayDino
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Audio)]
+    [ActionTarget(typeof(AudioSource), "gameObject")]
+    [ActionTarget(typeof(AudioClip), "oneShotClip")]
+	[Tooltip("Plays the Audio Clip set with Set Audio Clip or in the Audio Source inspector on a Game Object. Optionally plays a one shot Audio Clip./n If you wnat to change values while playing Wait For End Of Clip must be turned on")]
+	public class AudioPlayAdvanced : FsmStateAction
+	{
+		[RequiredField]
+		[CheckForComponent(typeof(AudioSource))]
+		[Tooltip("The GameObject with an AudioSource component.")]
+		public FsmOwnerDefault gameObject;
+		
+		[ObjectType(typeof(AudioClip))]
+		[Tooltip("Optionally play a 'one shot' AudioClip.")]
+		public FsmObject oneShotClip;
+		
+		[ActionSection("Settings")]
+		
+		[HasFloatSlider(0,255)]
+        [Tooltip("Set the priority.")]
+		public FsmInt priority;
+		
+		[HasFloatSlider(0,1)]
+        [Tooltip("Set the volume.")]
+		public FsmFloat volume;
+		
+		[HasFloatSlider(-1,1)]
+        [Tooltip("Set the Stereo Pan.")]
+		public FsmFloat stereoPan;
+		
+		[HasFloatSlider(0,1)]
+        [Tooltip("Set the Spatial Blend.")]
+		public FsmFloat spatialBlend;
+		
+		[HasFloatSlider(0,1.1f)]
+        [Tooltip("Set the reverb Zone Mix.")]
+		public FsmFloat reverbZoneMix;
+		
+		[ActionSection("3D Sound Settings.")]
+		
+		[HasFloatSlider(0,5)]
+        [Tooltip("Set the dopplerLevel.")]
+		public FsmFloat dopplerLevel;
+		
+		[ActionSection("Finish Options")]
+		
+		public bool WaitForEndOfClip;
+		
+		[Tooltip("Event to send when start playing or when the AudioClip finishes playing.")]
+		public FsmEvent finishedEvent;
+
+		private AudioSource audio;
+				
+		public override void Reset()
+		{
+			gameObject = null;
+			volume = new FsmFloat(){UseVariable=true};
+			priority = new FsmInt(){UseVariable=true};
+			stereoPan = new FsmFloat(){UseVariable=true};
+			spatialBlend = new FsmFloat(){UseVariable=true};
+			reverbZoneMix = new FsmFloat(){UseVariable=true};
+			dopplerLevel = new FsmFloat(){UseVariable=true};
+			oneShotClip = null;
+		    finishedEvent = null;
+			WaitForEndOfClip = false;
+		}
+
+		public override void OnEnter()
+		{
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go != null)
+			{
+				// cache the AudioSource component
+
+			    audio = go.GetComponent<AudioSource>();
+				if (audio != null)
+				{
+					if (!priority.IsNone)
+					{
+					audio.priority = priority.Value;
+					}
+					
+					if (!stereoPan.IsNone)
+					{
+					audio.panStereo = stereoPan.Value;
+					}
+					
+					if (!spatialBlend.IsNone)
+					{
+					audio.spatialBlend = spatialBlend.Value;
+					}
+					
+					if (!reverbZoneMix.IsNone)
+					{
+					audio.reverbZoneMix = reverbZoneMix.Value;
+					}
+					
+					if (!dopplerLevel.IsNone)
+					{
+					audio.dopplerLevel = dopplerLevel.Value;
+					}
+					
+					
+					var audioClip = oneShotClip.Value as AudioClip;
+
+					if (audioClip == null)
+					{	
+						audio.Play();
+						
+						if (!volume.IsNone)
+						{
+							audio.volume = volume.Value;
+						}
+						
+						return;
+					}
+					
+					if (!volume.IsNone)
+					{
+						audio.PlayOneShot(audioClip, volume.Value);
+					}
+					else
+					{
+						audio.PlayOneShot(audioClip);
+					}
+					if 	(!WaitForEndOfClip)
+					{
+						Fsm.Event(finishedEvent);
+						Finish();
+					}
+					return;
+				}
+			}
+			
+			// Finish if failed to play sound	
+		
+			Finish();
+		}
+		
+		public override void OnUpdate ()
+		{
+			if (audio == null)
+			{
+				Finish();
+			}
+			else
+			{
+				if (!audio.isPlaying)
+				{
+					Fsm.Event(finishedEvent);
+					Finish();
+				}
+				if (!volume.IsNone && volume.Value != audio.volume) audio.volume = volume.Value;
+				if (!priority.IsNone) audio.priority = priority.Value;
+				if (!stereoPan.IsNone) audio.panStereo = stereoPan.Value;
+				if (!spatialBlend.IsNone) audio.spatialBlend = spatialBlend.Value;
+				if (!reverbZoneMix.IsNone) audio.reverbZoneMix = reverbZoneMix.Value;
+				if (!dopplerLevel.IsNone) audio.dopplerLevel = dopplerLevel.Value;
+			}
+		}
+	}
+}
---
+++ Assets/PlayMaker Custom Actions/Audio/AudioPlaySimple.cs
@@ -0,0 +1,111 @@
+// (c) Copyright HutongGames, LLC 2010-2017. All rights reserved.
+/*--- __ECO__ __ACTION__ ---*/
+// Made By : DjayDino
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Audio)]
+    [ActionTarget(typeof(AudioSource), "gameObject")]
+    [ActionTarget(typeof(AudioClip), "oneShotClip")]
+	[Tooltip("Plays the Audio Clip set with Set Audio Clip or in the Audio Source inspector on a Game Object. Optionally plays a one shot Audio Clip.")]
+	public class AudioPlaySimple : FsmStateAction
+	{
+		[RequiredField]
+		[CheckForComponent(typeof(AudioSource))]
+		[Tooltip("The GameObject with an AudioSource component.")]
+		public FsmOwnerDefault gameObject;
+		
+		[HasFloatSlider(0,1)]
+        [Tooltip("Set the volume.")]
+		public FsmFloat volume;
+		
+		[ObjectType(typeof(AudioClip))]
+		[Tooltip("Optionally play a 'one shot' AudioClip.")]
+		public FsmObject oneShotClip;
+		
+		public FsmBool WaitForEndOfClip;
+		
+		[Tooltip("Event to send when the AudioClip finishes playing.")]
+		public FsmEvent finishedEvent;
+
+		private AudioSource audio;
+				
+		public override void Reset()
+		{
+			gameObject = null;
+			volume = 1f;
+			oneShotClip = null;
+		    finishedEvent = null;
+			WaitForEndOfClip = false;
+		}
+
+		public override void OnEnter()
+		{
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go != null)
+			{
+				// cache the AudioSource component
+
+			    audio = go.GetComponent<AudioSource>();
+				if (audio != null)
+				{
+					var audioClip = oneShotClip.Value as AudioClip;
+
+					if (audioClip == null)
+					{
+						audio.Play();
+						
+						if (!volume.IsNone)
+						{
+							audio.volume = volume.Value;
+						}
+						
+						return;
+					}
+					
+					if (!volume.IsNone)
+					{
+						audio.PlayOneShot(audioClip, volume.Value);
+					}
+					else
+					{
+						audio.PlayOneShot(audioClip);
+					}
+					if 	(WaitForEndOfClip.Value == false)
+					{
+						Fsm.Event(finishedEvent);
+						Finish();
+					}
+					
+					return;
+				}
+			}
+			
+			// Finish if failed to play sound	
+		
+			Finish();
+		}
+		
+		public override void OnUpdate ()
+		{
+			if (audio == null)
+			{
+				Finish();
+			}
+			else
+			{
+				if (!audio.isPlaying)
+				{
+					Fsm.Event(finishedEvent);
+					Finish();
+				}
+                else if (!volume.IsNone && volume.Value != audio.volume)
+				{
+					audio.volume = volume.Value;
+				}
+			}
+		}
+	}
+}
