193e18eade93ebc47d121c9a0ca2a61cc28f5e33
Jean Fabre <contact@fabrejean.net>
2017-03-24 09:10:03 +0300

    test

--- Assets/PlayMaker Custom Actions/__internal/PlayMakerActionsUtils.cs
+++
@@ -1,9 +0,0 @@
-// (c) Copyright HutongGames, LLC 2010-2013. All rights reserved.
-
-using UnityEngine;
-
-public class PlayMakerActionsUtils
-{
-	public enum EveryFrameUpdateSelector {OnUpdate,OnLateUpdate,OnFixedUpdate};
-
-}
