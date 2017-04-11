1aae398c44607c240382fac560119417f3082988
Jean Fabre <contact@fabrejean.net>
2017-04-11 09:28:59 +0300

    new action GetChildCountRecursive

---
+++ Assets/PlayMaker Custom Actions/GameObject/GetChildCountRecursive.cs
@@ -0,0 +1,57 @@
+// (c) Copyright HutongGames, LLC 2010-2017. All rights reserved.
+//--- __ECO__ __PLAYMAKER__ __ACTION__ ---//
+// Keywords: All Recursive Childs Children
+
+
+using UnityEngine;
+using System.Linq;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.GameObject)]
+	[Tooltip("Gets the number of children recursivly ( all children, not just direct childs) that a GameObject has.")]
+	public class GetChildCountRecursive : FsmStateAction
+	{
+		[RequiredField]
+        [Tooltip("The GameObject to test.")]
+		public FsmOwnerDefault gameObject;
+
+		[Tooltip("if true, will count inactive gameobjects")]
+		public FsmBool includeInactive;
+
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+        [Tooltip("Store the number of children in an int variable.")]
+		public FsmInt storeResult;
+
+		Transform[] _result;
+
+		public override void Reset()
+		{
+			gameObject = null;
+			includeInactive = null;
+			storeResult = null;
+		}
+
+		public override void OnEnter()
+		{
+			DoGetChildCountRecursive();
+			
+			Finish();
+		}
+
+		void DoGetChildCountRecursive()
+		{
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go == null)
+			{
+			    return;
+			}
+
+			_result = go.GetComponentsInChildren<Transform>(includeInactive.Value);
+
+			storeResult.Value = _result.Length -1;
+
+		}
+	}
+}
