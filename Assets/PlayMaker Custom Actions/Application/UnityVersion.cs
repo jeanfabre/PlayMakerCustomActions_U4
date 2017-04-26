535671aae8a87f95c52eaf047873b09d854efb56
djaydino <djaydino@gmail.com>
2017-04-26 10:59:50 +0200

    added action to show unity version in game

---
+++ Assets/PlayMaker Custom Actions/Application/UnityVersion.cs
@@ -0,0 +1,33 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2013. All rights reserved.
+
+using UnityEngine;
+using System.Collections;
+
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Application)]
+	[Tooltip("gets the unity version and place into a string")]
+	public class UnityVersion : FsmStateAction
+	{
+        [RequiredField]
+        [UIHint(UIHint.Variable)]
+        public FsmString Store;
+
+
+        public override void Reset()
+		{
+            Store = null;
+        }
+
+		public override void OnEnter()
+		{
+#if (UNITY_EDITOR)
+			    Store.Value = Application.unityVersion;
+#else
+            Store.Value = "This is not a unity Editor" ;
+#endif
+            Finish();
+		}
+	}
+}
