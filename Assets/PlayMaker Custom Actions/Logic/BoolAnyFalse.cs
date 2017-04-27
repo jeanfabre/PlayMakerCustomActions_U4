b95e59ee50df56d14fbfb35ffbc841400c9580cf
djaydino <djaydino@gmail.com>
2017-04-27 07:48:07 +0200

    added bool any false action

---
+++ Assets/PlayMaker Custom Actions/Logic/BoolAnyFalse.cs
@@ -0,0 +1,69 @@
+// (c) Copyright HutongGames, LLC 2010-2013. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+// made by DjayDino
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Logic)]
+	[Tooltip("Tests if any of the given Bool Variables are False.")]
+	public class BoolAnyFalse : FsmStateAction
+	{
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+        [Tooltip("The Bool variables to check.")]
+		public FsmBool[] boolVariables;
+
+        [Tooltip("Event to send if any of the Bool variables are False.")]
+		public FsmEvent sendEvent;
+		
+        [UIHint(UIHint.Variable)]
+        [Tooltip("Store the result in a Bool variable.")]
+        public FsmBool storeResult;
+
+        [Tooltip("Repeat every frame while the state is active.")]
+        public bool everyFrame;
+
+		public override void Reset()
+		{
+			boolVariables = null;
+			sendEvent = null;
+			storeResult = null;
+			everyFrame = false;
+		}
+
+		public override void OnEnter()
+		{
+			DoAnyTrue();
+			
+			if (!everyFrame)
+			{
+			    Finish();
+			}		
+		}
+		
+		public override void OnUpdate()
+		{
+			DoAnyTrue();
+		}
+		
+		void DoAnyTrue()
+		{
+			if (boolVariables.Length == 0)
+			{
+			    return;
+			}
+			
+			storeResult.Value = false;
+			
+			for (var i = 0; i < boolVariables.Length; i++) 
+			{
+				if (boolVariables[i].Value == false)
+				{
+					Fsm.Event(sendEvent);
+					storeResult.Value = true;
+					return;
+				}
+			}
+		}
+	}
+}
