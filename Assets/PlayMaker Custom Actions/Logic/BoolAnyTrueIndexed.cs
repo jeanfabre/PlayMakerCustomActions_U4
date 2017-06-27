f3eff5e543d13f88f16640a96b702c12008d4ee1
djaydino <djay_dino@hotmail.com>
2017-06-27 16:58:03 +0800

    new action bool any true indexed
    works the same as the original but it will give the element index

---
+++ Assets/PlayMaker Custom Actions/Logic/BoolAnyTrueIndexed.cs
@@ -0,0 +1,74 @@
+// (c) Copyright HutongGames, LLC 2010-2017. All rights reserved.
+// Made By DjayDino
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory(ActionCategory.Logic)]
+    [Tooltip("Tests if any of the given Bool Variables are True.")]
+    public class BoolAnyTrueIndexed : FsmStateAction
+    {
+        [RequiredField]
+        [UIHint(UIHint.Variable)]
+        [Tooltip("The Bool variables to check.")]
+        public FsmBool[] boolVariables;
+
+        [Tooltip("Event to send if any of the Bool variables are True.")]
+        public FsmEvent sendEvent;
+
+        [UIHint(UIHint.Variable)]
+        [Tooltip("Store the result in a Bool variable.")]
+        public FsmBool storeResult;
+
+        [UIHint(UIHint.Variable)]
+        [Tooltip("Store the result in a Bool variable.")]
+        public FsmInt elementIndex;
+
+        [Tooltip("Repeat every frame while the state is active.")]
+        public bool everyFrame;
+
+        public override void Reset()
+        {
+            boolVariables = null;
+            sendEvent = null;
+            storeResult = null;
+            everyFrame = false;
+        }
+
+        public override void OnEnter()
+        {
+            DoAnyTrue();
+
+            if (!everyFrame)
+            {
+                Finish();
+            }
+        }
+
+        public override void OnUpdate()
+        {
+            DoAnyTrue();
+        }
+
+        void DoAnyTrue()
+        {
+            if (boolVariables.Length == 0)
+            {
+                return;
+            }
+
+            storeResult.Value = false;
+
+            for (var i = 0; i < boolVariables.Length; i++)
+            {
+                if (boolVariables[i].Value)
+                {
+                    Fsm.Event(sendEvent);
+                    storeResult.Value = true;
+                    elementIndex.Value = i;
+
+                    return;
+                }
+            }
+        }
+    }
+}
