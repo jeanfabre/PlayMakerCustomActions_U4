7bcebd09a817fd444e38c0d19eeef76bdebc9a4c
Jean Fabre <contact@fabrejean.net>
2017-08-16 09:59:08 +0300

    new action GetChildCount2 with everyframe option

---
+++ Assets/PlayMaker Custom Actions/GameObject/GetChildCount2.cs
@@ -0,0 +1,53 @@
+// (c) Copyright HutongGames, LLC 2010-2013. All rights reserved.
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.GameObject)]
+	[Tooltip("Gets the number of children that a GameObject has. This version has everyframe option")]
+	public class GetChildCount2 : FsmStateAction
+	{
+		[RequiredField]
+        [Tooltip("The GameObject to test.")]
+		public FsmOwnerDefault gameObject;
+
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+        [Tooltip("Store the number of children in an int variable.")]
+		public FsmInt storeResult;
+
+		[Tooltip("Repeat every frame while the state is active.")]
+		public bool everyFrame;
+
+
+		public override void Reset()
+		{
+			gameObject = null;
+			storeResult = null;
+			everyFrame = false;
+		}
+		
+		public override void OnUpdate()
+		{
+			DoGetChildCount();
+			
+			if(!everyFrame)
+			{
+				Finish();
+			}
+		}
+
+
+		void DoGetChildCount()
+		{
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go == null)
+			{
+			    return;
+			}
+
+			storeResult.Value = go.transform.childCount;
+		}
+	}
+}
