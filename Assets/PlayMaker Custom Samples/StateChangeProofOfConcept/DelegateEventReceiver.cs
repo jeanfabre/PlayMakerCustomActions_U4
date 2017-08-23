d7a417c074879d1fa5fae015699db4988ef4ce88
Jean Fabre <contact@fabrejean.net>
2017-08-23 11:18:46 +0300

    - added some work ( not yet on the ecosystem)

---
+++ Assets/PlayMaker Custom Samples/StateChangeProofOfConcept/DelegateEventReceiver.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelegateEventReceiver : MonoBehaviour {
+	
+	public PlayMakerFsmStateWatcher stateWatcher;
+
+	public string previousState;
+	public string newState;
+
+	void Awake()
+	{
+		stateWatcher.onStateChangedEvent += HandleOnStateChangeEvent;
+	}
+	
+	void OnEnable()
+	{
+		stateWatcher.onStateChangedEvent -= HandleOnStateChangeEvent;
+		stateWatcher.onStateChangedEvent += HandleOnStateChangeEvent;
+	}
+	
+	void OnDisable()
+	{
+		stateWatcher.onStateChangedEvent -= HandleOnStateChangeEvent;
+	}
+
+	void HandleOnStateChangeEvent(string arg1, string arg2)
+	{
+		this.previousState = arg1;
+		this.newState = arg2;
+	}
+
+
+}
---
+++ Assets/PlayMaker Custom Samples/StateChangeProofOfConcept/UnityEventReceiver.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityEventReceiver : MonoBehaviour {
+
+	public string previousState;
+	public string newState;
+
+	public void OnFsmStateChanged(string previousState,string newState)
+	{
+		this.previousState = previousState;
+		this.newState = newState;
+	}
+
+}
