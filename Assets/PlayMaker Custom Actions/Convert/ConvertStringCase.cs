d700a67a3df245b872ec1a14ea1cf9a32ea6de4c
Jean Fabre <contact@fabrejean.net>
2017-10-19 11:32:57 +0300

    new action to convert string case

---
+++ Assets/PlayMaker Custom Actions/Convert/ConvertStringCase.cs
@@ -0,0 +1,70 @@
+// (c) Copyright HutongGames, LLC 2010-2017. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+
+using UnityEngine;
+using HutongGames.PlayMaker;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.String)]
+	[Tooltip("Converts a string to upper or lower case.")]
+	public class ConvertStringCase : FsmStateAction
+	{
+		public enum Case
+		{
+			Lower,
+			Upper
+		}
+		[RequiredField]
+		public FsmString String;
+		
+        [Tooltip("Select upper or lower case.")]
+		[ObjectType(typeof(Case))]
+		public FsmEnum operation;
+		
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+		public FsmString result;
+		
+		public bool everyFrame;
+
+		public override void Reset()
+		{
+			String = null;
+			result = null;
+			operation = null;
+			everyFrame = false;
+		}
+
+		public override void OnEnter()
+		{
+			DoSetStringValue();
+			
+			if (!everyFrame)
+				Finish();
+		}
+
+		public override void OnUpdate()
+		{
+			DoSetStringValue();
+		}
+		
+		void DoSetStringValue()
+		{
+			Case _case = (Case)operation.Value;
+			switch (_case)
+			{
+				case Case.Lower:
+					result.Value = String.Value.ToLower();
+					break;
+
+				case Case.Upper:
+					result.Value = String.Value.ToUpper();;
+					break;
+			}
+
+		}
+		
+	}
+}
