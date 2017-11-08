8b48b3c79f307221b55dc659492d1e89a55c53af
Jean Fabre <contact@fabrejean.net>
2017-11-08 10:28:55 +0300

    noise system (wip)

---
+++ Assets/PlayMaker Custom Actions/Texture/Editor/BuildNoiseTextureCustomEditor.cs
@@ -0,0 +1,48 @@
+﻿using HutongGames.PlayMaker.Actions;
+using HutongGames.PlayMakerEditor;
+using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+namespace HutongGames.PlayMakerEditor
+{
+	[CustomActionEditor(typeof(BuildNoiseTexture))]
+	public class BuildNoiseTextureCustomEditor : CustomActionEditor
+	{
+		BuildNoiseTexture _action;
+
+		public override bool OnGUI ()
+		{
+			_action = (BuildNoiseTexture)target;
+
+			EditField ("gameObject");
+			EditField ("resolution");
+			EditField ("frequency");
+			EditField ("octaves");
+			EditField ("lacunarity");
+			EditField ("persistence");
+			EditField ("dimensions");
+			EditField ("type");
+
+	
+			//SerializedObject serializedGradient = new SerializedObject(_action);
+
+			//this.
+			//SerializedProperty colorGradient = serializedGradient.FindProperty("coloring");
+
+			bool _changed;
+
+
+			EditorGUI.BeginChangeCheck();
+//EditorGUILayout.PropertyField(colorGradient, true, null);
+			if(EditorGUI.EndChangeCheck())
+			{
+			//	serializedGradient.ApplyModifiedProperties();
+			}
+
+			return false;
+		}
+
+
+	}
+}
