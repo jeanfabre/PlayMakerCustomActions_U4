8986d7d74593e5e375dc618fb6873c86a3af2402
Peter K <napstegucio124@googlemail.com>
2017-12-04 15:08:19 +0000

    Add files via upload

---
+++ Assets/PlayMaker Custom Actions/TablePro/Editor/GenerateTableActionEditor.cs
@@ -0,0 +1,40 @@
+﻿using Game.Scripts.CustomPlayMakerActions.TablePro;
+using HutongGames.PlayMaker.Actions;
+using HutongGames.PlayMakerEditor;
+using UnityEngine;
+
+[CustomActionEditor(typeof(GenerateTableAction))]
+public class GenerateTableActionEditor : XmlCustomActionEditor
+{
+
+    public override bool OnGUI()
+    {
+        bool edited = false;
+        GenerateTableAction _target = (GenerateTableAction)target;
+	
+        if (_target.xmlSource==null)
+        {
+            _target.xmlSource = new FsmXmlSource();
+        }
+	
+        if (_target.XPathHeaders==null)
+        {
+            _target.XPathHeaders = new FsmXpathQuery();
+        }
+
+	    if (_target.XPathRows == null)
+	    {
+		    _target.XPathRows = new FsmXpathQuery();
+	    }
+	
+        edited = edited || DataMakerActionEditorUtils.EditFsmXmlSourceField(_target.Fsm,_target.xmlSource);
+				
+        edited = edited || DataMakerActionEditorUtils.EditFsmXpathQueryField(_target.Fsm,_target.XPathHeaders);
+	    edited = edited || DataMakerActionEditorUtils.EditFsmXpathQueryField(_target.Fsm,_target.XPathRows);
+		
+        EditField("xmlResult");
+		
+        return GUI.changed || edited;
+    }
+	
+}
---
+++ Assets/PlayMaker Custom Actions/TablePro/GenerateTableAction.cs
@@ -0,0 +1,115 @@
+﻿using System.Diagnostics;
+using System.Xml;
+using HutongGames.PlayMaker;
+using HutongGames.PlayMaker.Actions;
+using SLS.Widgets.Table;
+
+namespace Game.Scripts.CustomPlayMakerActions.TablePro
+{
+	[ActionCategory("TablePro")]
+	[Tooltip("Generates a table based on header columns and rows provided from xml xpath")]
+	public class GenerateTableAction : DataMakerXmlActions
+	{
+		[ActionSection("XML Source")]
+		public FsmXmlSource xmlSource;
+
+		private XmlNodeList _headerNodelist;
+		private XmlNodeList _rowNodeList;
+		
+		[ActionSection("Table")]
+		
+		[RequiredField]
+		[Tooltip("The gameObject with the Table component")]
+		[CheckForComponent(typeof(Table))]
+		public FsmOwnerDefault gameObject;
+
+		[ActionSection("Headers")]
+		[RequiredField] 
+		[Tooltip("XPath for headers. For example table/tr/th")] 
+		public FsmXpathQuery XPathHeaders;
+
+		[ActionSection("Rows")]
+		[RequiredField]
+		[Tooltip("XPath for rows. For example table/tr[position()>1]")]
+		public FsmXpathQuery XPathRows;
+		
+		private Table _table;
+
+		public override void Reset()
+		{
+			XPathHeaders = null;
+			XPathRows = null;
+			xmlSource = null;
+			_headerNodelist = null;
+			_rowNodeList = null;
+			gameObject = null;
+			_table = null;
+		}
+
+		public override void OnEnter()
+		{
+			GenerateTable();
+			Finish();
+		}
+
+		private void GenerateTable()
+		{
+			if (xmlSource.Value == null)
+			{
+				LogWarning("Xml Source is empty, or likely invalid");
+			}
+
+			var targetObject = Fsm.GetOwnerDefaultTarget(gameObject);
+			
+			if (targetObject == null)
+			{
+				return;
+			}
+
+			_table = targetObject.GetComponent<Table>();
+			
+			if (_table != null)
+			{
+				_table.ResetTable();
+
+				// Add Column Headers
+
+				_headerNodelist = xmlSource.Value.SelectNodes(XPathHeaders.ParseXpathQuery(this.Fsm));
+				if (_headerNodelist == null) { LogWarning("Headers xpath is empty, or likely invalid"); return; }
+				
+				for (int i = 0; i < _headerNodelist.Count; i++)
+				{
+					var header = _headerNodelist[i];
+					_table.AddTextColumn(header.InnerText);
+				}
+				
+				// Initialize the table
+				_table.Initialize();
+				
+				_table.data.Clear();
+				
+				_rowNodeList = xmlSource.Value.SelectNodes(XPathRows.ParseXpathQuery(this.Fsm));
+				if (_rowNodeList == null) { LogWarning("Rows xpath is empty, or likely invalid"); return; }
+
+				for (int i = 0; i < _rowNodeList.Count; i++)
+				{
+					var rowData = _rowNodeList[i].ChildNodes;
+					var d = Datum.Body(i.ToString());
+					for (int j = 0; j < rowData.Count; j++)
+					{
+						d.elements.Add(rowData[j].InnerText);
+					}
+			
+					_table.data.Add(d);
+				}
+		
+				_table.StartRenderEngine();
+				
+			}
+			else
+			{
+				LogError("There is no table component on this gameobject.");
+			}
+		}
+	}
+}
