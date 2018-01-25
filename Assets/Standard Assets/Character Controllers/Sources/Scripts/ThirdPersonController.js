8ed5bba4f686c5926981d16fce1db8606819c6b0
Jean Fabre <contact@fabrejean.net>
2018-01-25 10:06:49 +0300

    standard assets, getnextRaycastAllHit, pool and singleton secure commit

--- Assets/Standard Assets/Character Controllers/Sources/Scripts/ThirdPersonController.js
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/ThirdPersonController.js
@@ -190,7 +190,7 @@ function UpdateSmoothedMovementDirection ()
 		_characterState = CharacterState.Idle;
 		
 		// Pick speed modifier
-		if (Input.GetKey (KeyCode.LeftShift) | Input.GetKey (KeyCode.RightShift))
+		if (Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift))
 		{
 			targetSpeed *= runSpeed;
 			_characterState = CharacterState.Running;
