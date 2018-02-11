da856d412cfb58605225af87204eb80fc976f68c
djaydino <djaydino@gmail.com>
2018-02-11 12:12:32 +0100

    Action Get Vector3 Length, original action "Get Vector Length" does not have 'Every Frame'

---
+++ Assets/PlayMaker Custom Actions/Vector3/GetVector3Length.cs
@@ -0,0 +1,46 @@
+// (c) Copyright HutongGames, LLC 2010-2013. All rights reserved.
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Vector3)]
+	[Tooltip("Get Vector3 Length.")]
+	public class GetVector3Length : FsmStateAction
+	{
+		public FsmVector3 vector3;
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+		public FsmFloat storeLength;
+        [Tooltip("Repeat every frame")]
+        public bool everyFrame;
+
+        public override void Reset()
+		{
+			vector3 = null;
+			storeLength = null;
+            everyFrame = false;
+        }
+
+		public override void OnEnter()
+		{
+			DoVectorLength();
+            if (!everyFrame)
+            {
+                Finish();
+            }
+        }
+
+        public override void OnUpdate()
+        {
+            DoVectorLength();
+        }
+
+        void DoVectorLength()
+		{
+			if (vector3 == null) return;
+			if (storeLength == null) return;
+			storeLength.Value = vector3.Value.magnitude;
+		}
+	}
+}
