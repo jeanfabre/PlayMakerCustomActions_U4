15af39cb06c09de2c86582f5b7bf6ae235c1fde6
djaydino <djaydino@gmail.com>
2018-02-12 10:50:00 +0100

    Addded New action 'Scene Objects Count'

---
+++ Assets/PlayMaker Custom Actions/GameObject/SceneObjectsCount.cs
@@ -0,0 +1,54 @@
+// (c) Copyright HutongGames, LLC 2010-2013. All rights reserved.
+// __ECO__ __PLAYMAKER__ __ACTION__
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory(ActionCategory.GameObject)]
+    [Tooltip("Gets the number of children that a GameObject has.")]
+    public class SceneObjectsCount : FsmStateAction
+    {
+
+        [RequiredField]
+        [UIHint(UIHint.Variable)]
+        [Tooltip("Store the number of children in an int variable.")]
+        public FsmInt storeResult;
+
+
+
+        [ActionSection("Optional")]
+        [Tooltip("Optional Find with tag")]
+        [UIHint(UIHint.Tag)]
+        public FsmString findWithTag;
+
+        private GameObject[] theGameObjects;
+
+        public override void Reset()
+        {
+            findWithTag = null;
+            storeResult = null;
+        }
+
+        public override void OnEnter()
+        {
+            DoSceneObjectsCount();
+
+            Finish();
+        }
+
+        void DoSceneObjectsCount()
+        {
+            if (string.IsNullOrEmpty(findWithTag.Value) || findWithTag.IsNone)
+            {
+                theGameObjects = GameObject.FindObjectsOfType(typeof(GameObject)) as GameObject[];
+                storeResult.Value = theGameObjects.Length;
+            }
+            else
+            {
+                theGameObjects = GameObject.FindGameObjectsWithTag(findWithTag.Value) as GameObject[];
+                storeResult.Value = theGameObjects.Length;
+            }
+        }
+    }
+}
