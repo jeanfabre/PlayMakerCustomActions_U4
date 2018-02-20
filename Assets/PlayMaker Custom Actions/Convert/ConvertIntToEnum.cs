0ac04ada635af5892cde222cdfe3fec99f56655f
Jean Fabre <contact@fabrejean.net>
2018-02-20 09:39:47 +0300

    New Action ConvertIntToEnum

---
+++ Assets/PlayMaker Custom Actions/Convert/ConvertIntToEnum.cs
@@ -0,0 +1,66 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2017. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using System;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Convert)]
+	[Tooltip("Convert an int value to an Enum Variable")]
+	public class ConvertIntToEnum: FsmStateAction
+	{
+		[Tooltip("The int value to set the variable to.")]
+		public FsmInt value;
+
+		[UIHint(UIHint.Variable)]
+		[Tooltip("The Enum Variable to set.")]
+		public FsmEnum enumVariable;
+
+		[Tooltip("Event Fired if conversion failed, that is the int is not found in the enum variable")]
+		public FsmEvent errorEvent;
+
+		[Tooltip("Repeat every frame.")]
+		public bool everyFrame;
+
+		int _valueCache;
+
+		public override void Reset()
+		{
+			enumVariable = null;
+			value = null;
+			errorEvent = null;
+			everyFrame = false;
+		}
+		
+		public override void OnEnter()
+		{
+			DoSetEnumValue();
+			
+			if (!everyFrame)
+			{
+				Finish();
+			}
+		}
+		
+		public override void OnUpdate()
+		{
+			DoSetEnumValue();
+		}
+		
+		void DoSetEnumValue()
+		{
+
+			if (Enum.IsDefined (enumVariable.Value.GetType (), value.Value)) {
+				enumVariable.Value = (Enum)Enum.Parse (enumVariable.Value.GetType (), value.Value.ToString ());
+			} else {
+
+				if (_valueCache!= value.Value)
+				{
+					_valueCache = value.Value;
+					Fsm.Event(errorEvent);
+				}
+			}
+		}
+		
+	}
+}
