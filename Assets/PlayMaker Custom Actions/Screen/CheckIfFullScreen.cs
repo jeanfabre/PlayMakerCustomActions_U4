201c7fa34555b0881d44e4658b2084e455235be3
Jean Fabre <contact@fabrejean.net>
2018-02-27 09:20:16 +0300

    New action to check if application is in fullscreen mode or not

---
+++ Assets/PlayMaker Custom Actions/Screen/CheckIfFullScreen.cs
@@ -0,0 +1,71 @@
+﻿// (c) copyright HutongGames, LLC 2010-2018. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+
+using UnityEngine;
+using System.Collections;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Screen)]
+	[Tooltip("Checks if screen is full screen")]
+	public class CheckIfFullScreen : FsmStateAction
+	{
+		[Tooltip("True if Application is full screen")]
+		public FsmBool isFullScreen;
+
+		[Tooltip("Event to send if application is full screen")]
+		public FsmEvent isFullScreenEvent;
+		
+		[Tooltip("Event to send if application is not full screen")]
+		public FsmEvent isNotFullScreenEvent;
+
+		[Tooltip("Runs everyframe. Event are sent discretly, on action first execution, and then only when changes occur")]
+		public bool everyFrame;
+
+
+		int _cache;
+
+		public override void Reset()
+		{
+			isFullScreen = null;
+			isFullScreenEvent = null;
+			isNotFullScreenEvent = null;
+			_cache = -1;
+		}
+		
+		public override void OnEnter()
+		{
+			_cache = -1;
+
+			Check ();
+
+			if (!everyFrame) {
+				Finish();
+			}
+		}
+
+		public override void OnUpdate ()
+		{
+			Check ();
+		}
+
+		void Check()
+		{
+			if (Screen.fullScreen && _cache != 1) {
+				_cache = 1;
+				isFullScreen.Value = true;
+				Fsm.Event(isFullScreenEvent);
+				return;
+			}
+
+			if (! Screen.fullScreen && _cache != 0) {
+				_cache = 0;
+				isFullScreen.Value = false;
+				Fsm.Event(isNotFullScreenEvent);
+				return;
+			}
+		}
+
+	}
+}
