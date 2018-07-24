58a19f5393949e4bfafbf0fda0798ccff896c797
Jean Fabre <contact@fabrejean.net>
2018-07-24 10:28:17 +0300

    New ActionSetFsmKeepDelayedEventsOnStateExit

---
+++ Assets/PlayMaker Custom Actions/StateMachine/SetFsmKeepDelayedEventsOnStateExit.cs
@@ -0,0 +1,28 @@
+// (c) Copyright HutongGames, LLC 2010-2018. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory(ActionCategory.StateMachine)]
+	[Tooltip("Raise the KeepDelayedEventsOnStateExit flag so that delayed event are kept even when state exited")]
+	public class SetFsmKeepDelayedEventsOnStateExit : FsmStateAction
+    {
+		[UIHint(UIHint.TextArea)]
+        [Tooltip("If True, delayed event will be kept if the state exits")]
+		public FsmBool keepDelayedEvents;
+
+		public override void Reset ()
+		{
+			keepDelayedEvents = null;
+		}
+
+        public override void OnEnter()
+        {
+			this.Fsm.KeepDelayedEventsOnStateExit = keepDelayedEvents.Value;
+
+            Finish ();
+        }
+    }
+}
