46462cde9ea4d340ee783b8aa949f5ea612c08e0
Jean Fabre <contact@fabrejean.net>
2018-09-04 09:20:53 +0300

    New Action ReleaseRenderTexture

---
+++ Assets/PlayMaker Custom Actions/Camera/ReleaseRenderTexture.cs
@@ -0,0 +1,53 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2018. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Camera)]
+	[Tooltip("Release a RenderTexture")]
+	public class ReleaseRenderTexture : FsmStateAction
+	{
+
+		[CheckForComponent(typeof(Camera))]
+		public FsmOwnerDefault camera;
+
+		[UIHint(UIHint.Variable)]
+		[Tooltip("The renderTexture to release")]
+		public FsmTexture OrRenderTexture;
+
+		public override void Reset()
+		{
+			camera = null;
+			OrRenderTexture = null;
+		}
+		
+		public override void OnEnter()
+		{
+
+			GameObject go = camera.OwnerOption == OwnerDefaultOption.UseOwner ? Owner : camera.GameObject.Value;
+			if (go == null) {
+				LogError ("camera is missing!");
+				return;
+			} else {
+				Camera _c = go.GetComponent<Camera>() ;
+				if (_c!=null && _c.targetTexture !=null)
+				{
+					_c.targetTexture.Release();
+				}
+			}
+
+			if (OrRenderTexture.Value != null) {
+				RenderTexture _t = 	OrRenderTexture.Value as RenderTexture;
+				if (_t!=null)
+				{
+					_t.Release();
+				}
+			}
+			Finish();
+		}
+
+
+	}
+}
