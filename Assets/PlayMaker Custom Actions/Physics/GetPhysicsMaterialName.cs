a851c8be770e5a607914de89992282b00e5aac92
Jean Fabre <contact@fabrejean.net>
2018-09-10 09:47:21 +0300

    New Actions GetPhysicsMaterialNamer and GetPhysics2dMaterialName

---
+++ Assets/PlayMaker Custom Actions/Physics/GetPhysicsMaterialName.cs
@@ -0,0 +1,97 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2018. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Physics)]
+	[Tooltip("Gets the name of game object's Physics material.")]
+	public class GetPhysicsMaterialName : FsmStateAction
+	{
+		[Tooltip("The GameObject that the Physcismaterial is applied to ( requires a Collider).")]
+		[CheckForComponent(typeof(Collider))]
+		public FsmOwnerDefault gameObject;
+		
+		[Tooltip("Alternatively specify a Physics Material instead of a GameObject and Index.")]
+		[ObjectType(typeof(PhysicMaterial))]
+		public FsmObject material;
+
+		[Tooltip("If the pointer to the material is an instance, the name will be appended with ' (instance)'. If True, this will be removed, else the name will be as is")]
+		public bool trimName;
+
+		[Tooltip("The name of the material.")]
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+		public FsmString materialName;
+
+		[Tooltip("Event sent if material was not found")]
+		public FsmEvent fail;
+
+		string _token = " (instance)";
+
+		Collider _c;
+
+		public override void Reset()
+		{
+			gameObject = null;
+			material = null;
+
+			trimName = true;
+			materialName = null;
+			fail = null;
+		}
+		
+		public override void OnEnter()
+		{
+			bool ok = DoGetMaterialName();
+
+			if (!ok) Fsm.Event(fail);
+
+			Finish();
+		}
+		
+		bool DoGetMaterialName()
+		{
+			if (materialName.IsNone)
+			{
+				return false;
+			}
+
+			if (material.Value != null)
+			{
+				materialName.Value = material.Value.name;
+				return true;
+			}
+
+
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go == null) return false;
+
+			_c = go.GetComponent<Collider> ();
+
+			if (_c == null)
+			{
+				LogError("Missing Collider!");
+				return false;
+			}
+
+
+			if (_c.material != null) {
+				materialName.Value = parseMaterialName(_c.material.name);
+				return true;
+			}
+
+			return false;
+		}
+
+		string parseMaterialName(string name)
+		{
+			if ( trimName && name.EndsWith(_token))
+			{
+				return name.Replace(_token,"");
+			}
+			return name;
+		}
+	}
+}
---
+++ Assets/PlayMaker Custom Actions/Physics2D/GetPhysics2dMaterialName.cs
@@ -0,0 +1,84 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2018. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Physics)]
+	[Tooltip("Gets the name of game object's Physics2D material.")]
+	public class GetPhysics2dMaterialName : FsmStateAction
+	{
+		[Tooltip("The GameObject that the Physics material is applied to ( requires a Collider2D).")]
+		[CheckForComponent(typeof(Collider2D))]
+		public FsmOwnerDefault gameObject;
+		
+		[Tooltip("Alternatively specify a Physics Material 2D instead of a GameObject and Index.")]
+		[ObjectType(typeof(PhysicsMaterial2D))]
+		public FsmObject material;
+
+		[Tooltip("The name of the material.")]
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+		public FsmString materialName;
+
+		[Tooltip("Event sent if material was not found")]
+		public FsmEvent fail;
+
+		string _token = " (instance)";
+
+		Collider2D _c;
+
+		public override void Reset()
+		{
+			gameObject = null;
+			material = null;
+
+			materialName = null;
+			fail = null;
+		}
+		
+		public override void OnEnter()
+		{
+			bool ok = DoGetMaterialName();
+
+			if (!ok) Fsm.Event(fail);
+
+			Finish();
+		}
+		
+		bool DoGetMaterialName()
+		{
+			if (materialName.IsNone)
+			{
+				return false;
+			}
+
+			if (material.Value != null)
+			{
+				materialName.Value = material.Value.name;
+				return true;
+			}
+
+
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go == null) return false;
+
+			_c = go.GetComponent<Collider2D> ();
+
+			if (_c == null)
+			{
+				LogError("Missing Collider!");
+				return false;
+			}
+
+			if (_c.sharedMaterial != null) {
+				materialName.Value = _c.sharedMaterial.name;
+				return true;
+			}
+
+			return false;
+		}
+
+	}
+}
