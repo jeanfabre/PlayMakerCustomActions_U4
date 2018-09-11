c6c4769bc16ac9e1dd80ca368caebe016f52f39c
Jean Fabre <contact@fabrejean.net>
2018-09-11 09:09:56 +0300

    new Action GetMaterials

---
+++ Assets/PlayMaker Custom Actions/Material/GetMaterials.cs
@@ -0,0 +1,59 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2018. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Material)]
+	[Tooltip("Gets all material of a GameObject with a Renderer component")]
+	public class GetMaterials : FsmStateAction
+	{
+		[Tooltip("The GameObject that the material is applied to.")]
+		[CheckForComponent(typeof(Renderer))]
+		public FsmOwnerDefault gameObject;
+
+		[Tooltip("All materials on the GameObject's Renderer component")]
+		[ArrayEditor(VariableType.Material)]
+		[UIHint(UIHint.Variable)]
+		public FsmArray materials;
+
+		[Tooltip("Event sent if material was not found")]
+		public FsmEvent fail;
+
+		string _token = " (instance)";
+		
+		public override void Reset()
+		{
+			gameObject = null;
+			materials = null;
+			fail = null;
+		}
+		
+		public override void OnEnter()
+		{
+			bool ok = DoGetMaterialName();
+
+			if (!ok) Fsm.Event(fail);
+
+			Finish();
+		}
+		
+		bool DoGetMaterialName()
+		{
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go == null) return false;
+			
+			if (go.renderer == null)
+			{
+				LogError("Missing Renderer!");
+				return false;
+			}
+
+			materials.objectReferences = go.renderer.materials;
+
+			return true;
+		}
+
+	}
+}
