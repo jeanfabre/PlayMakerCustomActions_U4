b2063b90d87aa79b8d048e6dfcc256c535318e5d
Jean Fabre <contact@fabrejean.net>
2018-10-18 11:25:08 +0300

    new action Vector3ProjectOnPlane

---
+++ Assets/PlayMaker Custom Actions/Vector3/Vector3ProjectOnPlane.cs
@@ -0,0 +1,58 @@
+// (c) Copyright HutongGames, LLC 2010-2013. All rights reserved.
+// --- __ECO__ __PLAYMAKER__ __ACTION__ 
+// https://hutonggames.com/playmakerforum/index.php?topic=19299.msg84027
+
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Vector3)]
+	[Tooltip("Performs projection of a vector on a plane using its normal.")]
+	public class Vector3ProjectOnPlane : FsmStateAction
+	{
+		[RequiredField]
+		[Tooltip("The Vector to project")]
+		public FsmVector3 vector;
+
+		[RequiredField]
+		[Tooltip("the Plane normal")]
+		public FsmVector3 planeNormal;
+
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+		[Tooltip("the resulting projection")]
+		public FsmVector3 storeVector3Result;
+
+		[Tooltip("Runs every frame")]
+		public bool everyFrame;
+
+		public override void Reset()
+		{
+			vector = null;
+			planeNormal = null;
+			storeVector3Result = null;
+			everyFrame = false;
+		}
+
+		public override void OnEnter()
+		{
+			DoProjectOnPlane();
+
+			if (!everyFrame)
+			{
+				Finish();
+			}
+		}
+
+		public override void OnUpdate()
+		{
+			DoProjectOnPlane();
+		}
+
+		void DoProjectOnPlane()
+		{
+            storeVector3Result.Value = Vector3.ProjectOnPlane(vector.Value, planeNormal.Value);
+		}
+	}
+}
