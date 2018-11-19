2e13149fecd324eb9d3e0641a41d891daeba31aa
Fat Pug Studio <Oshigawa@users.noreply.github.com>
2018-11-19 14:04:34 +0100

    Add files via upload

---
+++ Assets/PlayMaker Custom Actions/Physics2D/SetCircleCollider2DRadius.cs
@@ -0,0 +1,56 @@
+﻿// License: Attribution 4.0 International (CC BY 4.0)
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory(ActionCategory.Physics)]
+    [Tooltip("Set the size of a Circle Collider 2D")]
+    [HelpUrl("")]
+    public class SetCircleCollider2DRadius : ComponentAction<Rigidbody>
+    {
+        [RequiredField]
+        [CheckForComponent(typeof(CircleCollider2D))]
+        [Tooltip("The GameObject to apply the size to.")]
+        public FsmOwnerDefault gameObject;
+	
+		[Tooltip("Circle radius")]
+        public FsmFloat radius;
+
+        [Tooltip("Repeat every frame while the state is active.")]
+        public FsmBool everyFrame;
+
+        public override void Reset()
+        {
+            gameObject = null;
+			radius = null;
+            everyFrame = false;
+        }
+
+
+        public override void OnEnter()
+        {
+            DoChange();
+
+            if (!everyFrame.Value)
+            {
+                Finish();
+            }
+        }
+
+        public override void OnFixedUpdate()
+        {
+            DoChange();
+        }
+
+        void DoChange()
+        {
+            var go = Fsm.GetOwnerDefaultTarget(gameObject);
+            CircleCollider2D collider = go.GetComponent<CircleCollider2D>();
+			collider.radius = radius.Value;
+                   
+
+        }
+    }
+}
